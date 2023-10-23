6bc18b72cd5ce42cc7aa47fc22c8d3ecad0e7908
underscope <underscope@gmail.com>
2023-10-23 13:16:07 +0200

    💄

--- extensions/content-elements/install.js
+++ extensions/content-elements/install.js
@@ -1,26 +1,26 @@
-// TODO: Add to schema after installation
-import inquirer from "inquirer";
-import shell from "shelljs";
-import jsonfile from 'jsonfile'
+// TODO: Add to the schema after installation
+import inquirer from 'inquirer';
+import jsonfile from 'jsonfile';
+import shell from 'shelljs';
 
 // Prompt for element name and package names
 const { name, clientPackage, serverPackage } = await inquirer.prompt([
   {
-    type: "input",
-    name: "name",
-    message: "Enter element name (kebab-case):",
+    type: 'input',
+    name: 'name',
+    message: 'Enter element name (kebab-case):',
   },
   {
-    type: "input",
-    name: "clientPackage",
+    type: 'input',
+    name: 'clientPackage',
     message:
-      "Provide the name of the package that contains the edit component:",
+      'Provide the name of the package that contains the edit component:',
   },
   {
-    type: "input",
-    name: "serverPackage",
+    type: 'input',
+    name: 'serverPackage',
     message:
-      "Provide the name of the package that contains the server component:",
+      'Provide the name of the package that contains the server component:',
   },
 ]);
 // Install packages
@@ -33,7 +33,7 @@ await shell.exec(
 await shell.exec(
   `npx hygen server-package new ${name} --packageName ${serverPackage}`
 );
-// Add to registry
+// Add to the content element registry
 const elements = await jsonfile.readFile('./registry.json');
 elements.push({ name, clientPackage, serverPackage });
 await jsonfile.writeFile('./registry.json', elements);
