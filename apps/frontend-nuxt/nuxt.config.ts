412c78acf59f5c8677afaf5699a72592014d3833
underscope <underscope@gmail.com>
2023-11-10 10:43:56 +0100

    Set nuxt port to 8081

--- apps/frontend-nuxt/nuxt.config.ts
+++ apps/frontend-nuxt/nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  devServer: {
+    // TODO: Baked in until frontend is migrated to Nuxt
+    port: 8081,
+  },
 });
