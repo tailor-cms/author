69816ddf8588f29058546d59d460d1e70e405726
underscope <underscope@gmail.com>
2023-11-14 12:51:02 +0100

    Configure vuetify theme

--- apps/frontend-nuxt/plugins/vuetify.ts
+++ apps/frontend-nuxt/plugins/vuetify.ts
@@ -1,12 +1,28 @@
-import { createVuetify } from 'vuetify';
+import type { ThemeDefinition } from 'vuetify';
+
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import { createVuetify } from 'vuetify';
+
+const defaultTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    primary: '#607D8B',
+    secondary: '#E91E63',
+  },
+};
 
 export default defineNuxtPlugin((nuxt) => {
   const vuetify = createVuetify({
     components,
     directives,
     ssr: true,
+    theme: {
+      defaultTheme: 'default',
+      themes: {
+        default: defaultTheme,
+      },
+    },
   });
   nuxt.vueApp.use(vuetify);
 });
