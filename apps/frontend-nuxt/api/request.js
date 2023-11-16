08c5fda485f1cb8f95b60d21ff4dfe2e49d7a0dd
underscope <underscope@gmail.com>
2023-11-16 20:42:32 +0100

    Migrate API client

---
+++ apps/frontend-nuxt/api/request.js
@@ -0,0 +1,55 @@
+import axios, { Axios } from 'axios';
+import buildFullPath from 'axios/unsafe/core/buildFullPath';
+
+Axios.prototype.submitForm = function (url, fields, options) {
+  const action = buildFullPath(this.defaults.baseURL, url);
+  return Promise.resolve(submitForm(action, fields, options));
+};
+
+const config = {
+  baseURL: '/api',
+  withCredentials: true,
+  headers: { 'Content-Type': 'application/json' },
+};
+
+// Instance of axios to be used for all API requests.
+const client = axios.create(config);
+
+// Attach additional instance without interceptors
+Object.defineProperty(client, 'base', {
+  get() {
+    if (!this.base_) this.base_ = axios.create(config);
+    return this.base_;
+  },
+});
+
+const isAuthError = (err) => [401, 403].includes(err.response?.status);
+
+client.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (isAuthError(err)) {
+      const isAuthenticated = useCookie('isAuthenticated');
+      isAuthenticated.value = false;
+      const authRoute = '/auth';
+      if (process.server) return navigateTo(authRoute);
+      return window.location.replace('/auth');
+    }
+    throw err;
+  },
+);
+
+export default client;
+
+function submitForm(action, fields = {}, options) {
+  const form = document.createElement('form');
+  Object.assign(form, { method: 'POST', target: 'blank', action }, options);
+  Object.entries(fields).forEach(([name, attrs]) => {
+    const input = document.createElement('input');
+    Object.assign(input, { name }, attrs);
+    form.appendChild(input);
+  });
+  document.body.appendChild(form);
+  form.submit();
+  form.remove();
+}
