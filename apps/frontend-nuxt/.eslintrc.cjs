93850bd0faa35dfecb23c002fdb5626155072c5b
underscope <underscope@gmail.com>
2023-11-17 19:51:36 +0100

    Add eslint config

---
+++ apps/frontend-nuxt/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+  },
+  extends: [
+    '@tailor-cms/eslint-config',
+    '@nuxtjs/eslint-config-typescript',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['layouts/*.vue', 'pages/**/*.vue', 'error.vue'],
+      rules: { 'vue/multi-word-component-names': 'off' },
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: { parser: '@typescript-eslint/parser' },
+    },
+  ],
+};
