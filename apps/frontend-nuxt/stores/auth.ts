2de3518cd06412e31190609c0c19c71678898e79
underscope <underscope@gmail.com>
2023-11-19 21:17:43 +0100

    Add simple auth store

---
+++ apps/frontend-nuxt/stores/auth.ts
@@ -0,0 +1,55 @@
+import { auth as api } from '@/api';
+
+export const useAuthStore = defineStore('auth', () => {
+  const user = ref(null);
+
+  function $reset({ user = null } = {}) {
+    user.value = user;
+  }
+
+  function login(credentials: {
+    email: string;
+    password: string;
+  }): Promise<void> {
+    return api
+      .login(credentials)
+      .then(({ data: { user } }) => $reset({ user }));
+  }
+
+  function logout() {
+    return api.logout().then(() => $reset());
+  }
+
+  function changePassword({
+    currentPassword,
+    newPassword,
+  }: {
+    currentPassword: string;
+    newPassword: string;
+  }) {
+    return api.changePassword(currentPassword, newPassword);
+  }
+
+  function fetchUserInfo() {
+    return api
+      .getUserInfo()
+      .then(({ data: { user } }) => $reset({ user }))
+      .catch(() => $reset());
+  }
+
+  function updateInfo(payload: any) {
+    return api
+      .updateUserInfo(payload)
+      .then(({ data }) => (user.value = data.user));
+  }
+
+  return {
+    user,
+    login,
+    logout,
+    changePassword,
+    fetchUserInfo,
+    updateInfo,
+    $reset,
+  };
+});
