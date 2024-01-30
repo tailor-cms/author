41ee61985a519d22d2048546316cce76963bae8f
underscope <underscope@gmail.com>
2024-01-30 15:59:38 +0100

    Init main Toolbar

---
+++ apps/frontend-nuxt/components/common/Navbar.vue
@@ -0,0 +1,129 @@
+<template>
+  <VAppBar class="elevation-0" color="primary-darken-3" density="compact">
+    <NuxtLink to="/" class="app-brand pl-4">
+      <img
+        src="/img/default-logo-full.svg"
+        width="36px"
+        height="36px"
+        alt="Tailor logo"
+        class="pt-2"
+      />
+      <VAppBarTitle class="app-name">
+        Tailor
+        <span class="text-caption text-uppercase">author</span>
+      </VAppBarTitle>
+    </NuxtLink>
+    <template v-slot:append>
+      <VBtn
+        v-for="{ name, to } in routes"
+        :key="name"
+        :to="to"
+        class="mr-2"
+        color="secondary-lighten-4"
+        height="100"
+        variant="text"
+      >
+        <span class="toolbar-route text-truncate">{{ name }}</span>
+      </VBtn>
+      <VMenu
+        min-width="220px"
+        transition="slide-y-transition"
+        z-index="1000"
+        offset-y
+      >
+        <template v-slot:activator="{ props }">
+          <VBtn v-bind="props" icon class="mx-2">
+            <VAvatar size="36" color="teal-accent-4">
+              <img :src="user.imgUrl" width="34" />
+            </VAvatar>
+          </VBtn>
+        </template>
+        <VList class="text-left">
+          <VListItem>
+            <VListItemTitle>{{ user.email }}</VListItemTitle>
+          </VListItem>
+          <VListItem to="/">
+            <VListItemTitle>Profile</VListItemTitle>
+          </VListItem>
+          <VListItem @click="logout">
+            <VListItemTitle>Logout</VListItemTitle>
+          </VListItem>
+        </VList>
+      </VMenu>
+    </template>
+  </VAppBar>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useAuthStore } from '@/stores/auth';
+
+defineProps<{ user: any }>();
+
+const authStore = useAuthStore();
+
+// TODO: To be implemented
+const repository = computed(() => null) as any;
+const routes = computed(() => {
+  const items = [
+    { name: 'Catalog', to: '/' },
+    { name: 'Admin', to: '/admin' },
+  ];
+  if (!authStore.isAdmin) items.pop();
+  if (repository.value) {
+    items.unshift({
+      name: `${repository.value.name} structure`,
+      to: `repository/${repository.value?.id}`
+    });
+  }
+  return items;
+});
+
+const logout = async () => {
+  await authStore.logout();
+  navigateTo('/auth');
+};
+</script>
+
+<style lang="scss" scoped>
+$container-height: 2.5rem;
+$font-color: #333;
+
+.v-toolbar {
+  z-index: 10;
+
+  .v-toolbar__content .v-btn.v-btn--icon {
+    width: unset;
+    height: unset;
+  }
+
+  .v-btn:not(.v-btn--active) {
+    color: #fff !important;
+  }
+}
+
+.app-brand {
+  display: flex;
+  padding-bottom: 0.125rem;
+  text-decoration: none;
+  cursor: pointer;
+
+  .app-name {
+    margin: 0.125rem 0 0 0.375rem;
+    color: #fafafa;
+    font-size: 1.25rem;
+    font-weight: 400;
+    letter-spacing: 1px;
+    line-height: $container-height;
+    text-transform: uppercase;
+  }
+}
+
+.toolbar-route {
+  max-width: 12.5rem;
+}
+
+.v-avatar img {
+  padding: 0.125rem;
+}
+</style>
