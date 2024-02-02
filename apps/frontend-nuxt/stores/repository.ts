0c5bd263bc7bb30af8c10b824c777b684f0d75a5
underscope <underscope@gmail.com>
2024-02-02 23:11:35 +0100

    Add remaining repository store actions

--- apps/frontend-nuxt/stores/repository.ts
+++ apps/frontend-nuxt/stores/repository.ts
@@ -4,15 +4,50 @@ import type {
   Tag,
 } from '@/api/interfaces/repository';
 
-import { repository as api } from '@/api';
+import { repository as api, tag as tagApi } from '@/api';
+import repositoryFilterConfigs from '~/components/catalog/Filter/repositoryFilterConfigs';
 import { useAuthStore } from './auth';
+
+const getDefaultQueryParams = () => ({
+  offset: 0,
+  limit: 12,
+  search: '',
+  sortBy: {
+    field: 'updatedAt',
+    direction: 'DESC',
+  },
+  pinned: false,
+  filter: [],
+});
 
 export const useRepositoryStore = defineStore('repositories', () => {
   const authStore = useAuthStore();
 
   const $items = reactive(new Map<string, Repository>());
   const items = computed(() => Array.from($items.values()));
-  const tags = reactive([] as Tag[]);
+  const $tags = reactive(new Map<string, Tag>());
+  const tags = computed(() => Array.from($tags.values()));
+
+  const queryParams = reactive(getDefaultQueryParams());
+
+  const query = computed(() => {
+    const { sortBy, pinned, filter, ...rest } = queryParams;
+    const filters = filter.reduce((acc, { id, type }) => {
+      const filterTypeConfig = repositoryFilterConfigs[type] as any;
+      acc[filterTypeConfig.queryParam] ||= [];
+      acc[filterTypeConfig.queryParam].push(id);
+      return acc;
+    }, {} as any);
+    return {
+      ...rest,
+      sortBy: sortBy.field,
+      sortOrder: sortBy.direction,
+      ...filters,
+      ...{
+        pinned: pinned || undefined,
+      },
+    };
+  });
 
   function findById(id: number | string) {
     if (typeof id === 'string') return $items.get(id);
@@ -25,25 +60,43 @@ export const useRepositoryStore = defineStore('repositories', () => {
     return item;
   }
 
+  const create = async ({
+    schema,
+    name,
+    description,
+  }: {
+    schema: string;
+    name: string;
+    description: string;
+  }) => {
+    await api.save({ schema, name, description });
+  };
+
+  async function get(id: number): Promise<Repository> {
+    const repository: Repository = await api.get(id);
+    processRepository(repository);
+    add(repository);
+    return repository;
+  }
+
   async function fetch(): Promise<Repository[]> {
-    const repositories: Repository[] = await api.getRepositories();
+    const repositories: Repository[] = await api.getRepositories(query.value);
+    if (query.value.offset === 0) $items.clear();
     repositories.forEach((it) => add(it));
     return repositories;
   }
 
-  async function pin({ id, pin }: { id: number; pin: boolean }) {
-    const repositoryUser = (await api.pin(id, pin)) as RepositoryUser;
-    const repository = items.value.find((it) => it.id === id);
-    if (!repository) throw new Error('Repository not found!');
-    $items.set(repository.uid, { ...repository, repositoryUser });
-    return repositoryUser;
+  async function fetchTags(opts = { associated: true }) {
+    const tags: Tag[] = await tagApi.fetch(opts);
+    tags.forEach((it) => $tags.set(it.uid, it));
   }
 
   async function addTag(repositoryId: number, name: string) {
     const addedTag = await api.addTag({ repositoryId, name });
     const repository = findById(repositoryId);
     if (!repository) throw new Error('Repository not found!');
-    if (!tags.find((it) => it.id === addedTag.id)) tags.push(addedTag);
+    if (!tags.value.find((it) => it.id === addedTag.id))
+      $tags.set(addedTag.uid, addedTag);
     if (!repository.tags.find((it) => it.id === addedTag.id))
       repository.tags = [...repository.tags, addedTag];
   }
@@ -53,10 +106,23 @@ export const useRepositoryStore = defineStore('repositories', () => {
     const repository = findById(repositoryId);
     if (!repository) throw new Error('Repository not found!');
     repository.tags = repository.tags.filter((it) => it.id !== tagId);
+    if (!queryParams.filter.find((item: any) => item.id === tagId)) return;
+    queryParams.offset = 0;
+    await fetch();
+  }
+
+  async function pin({ id, pin }: { id: number; pin: boolean }) {
+    const repositoryUser: RepositoryUser = await api.pin(id, pin);
+    const repository = findById(id);
+    if (!repository) throw new Error('Repository not found!');
+    $items.set(repository.uid, { ...repository, repositoryUser });
+    return repositoryUser;
   }
 
   function $reset() {
     $items.clear();
+    $tags.clear();
+    Object.assign(queryParams, getDefaultQueryParams());
   }
 
   function processRepository(repository: Repository) {
@@ -70,12 +136,18 @@ export const useRepositoryStore = defineStore('repositories', () => {
   return {
     $items,
     items,
+    $tags,
+    tags,
     findById,
     add,
+    create,
+    get,
     fetch,
-    pin,
+    queryParams,
+    fetchTags,
     addTag,
     removeTag,
+    pin,
     $reset,
   };
 });
