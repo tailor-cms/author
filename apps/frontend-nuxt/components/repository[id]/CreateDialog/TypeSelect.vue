e7ff2ea75be1929a95adefb7818e0b800e03b674
underscope <underscope@gmail.com>
2024-02-07 15:01:45 +0100

    Add TypeSelection component

---
+++ apps/frontend-nuxt/components/repository[id]/CreateDialog/TypeSelect.vue
@@ -0,0 +1,33 @@
+<template>
+  <VSelect
+    :v-model="value"
+    :items="options"
+    :disabled="props.disabled"
+    :menu-props="{ offset: 10, maxHeight: 220 }"
+    item-title="label"
+    item-value="type"
+    name="type"
+    label="Type"
+    variant="outlined"
+    class="required"
+  >
+    <template v-slot:item="{ props, item: { raw } }">
+      <VListItem
+        v-bind="props"
+        :prepend-icon="`mdi-${hasSubtypes(raw) ? 'folder' : 'file-outline'}`"
+      />
+    </template>
+  </VSelect>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  value: { type: String, default: null },
+  options: { type: Array, required: true },
+  disabled: { type: Boolean, default: false },
+});
+
+const hasSubtypes = (outlineItemConfig: any) => {
+  return !!outlineItemConfig.subLevels?.length;
+};
+</script>
