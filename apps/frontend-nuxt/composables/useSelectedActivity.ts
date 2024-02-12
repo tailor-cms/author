79e7807b33b8c04896f12c660cecf80252d8c766
underscope <underscope@gmail.com>
2024-02-12 14:38:25 +0100

    Pass target activity to composable

--- apps/frontend-nuxt/composables/useSelectedActivity.ts
+++ apps/frontend-nuxt/composables/useSelectedActivity.ts
@@ -2,27 +2,25 @@ import { computed } from 'vue';
 import get from 'lodash/get';
 import InsertLocation from '@/lib/InsertLocation';
 import uniqBy from 'lodash/uniqBy';
+import type { StoreActivity } from '@/stores/activity';
 
 import { useCurrentRepository } from '@/stores/current-repository';
 
-export const useSelectedActivity = () => {
+export const useSelectedActivity = (activity: StoreActivity) => {
   const { $schemaService } = useNuxtApp() as any;
   const repoStore = useCurrentRepository();
   const activityStore = useActivityStore();
 
   const isEditable = computed(() => {
-    if (!repoStore.selectedActivity) return false;
-    return $schemaService.isEditable(repoStore.selectedActivity.type);
+    return $schemaService.isEditable(activity.type);
   });
 
   const parent = computed(() => {
-    if (!repoStore.selectedActivity) return;
-    const { parentId } = repoStore.selectedActivity;
+    const { parentId } = activity;
     return repoStore.outlineActivities.find((it) => it.id === parentId);
   });
 
   const sameLevel = computed(() => {
-    if (!repoStore.selectedActivity) return [];
     if (!parent.value)
       return repoStore.taxonomy.filter((it: any) => it.rootLevel);
     const { type } = parent.value;
@@ -34,8 +32,7 @@ export const useSelectedActivity = () => {
   });
 
   const subLevels = computed(() => {
-    if (!repoStore.selectedActivity) return [];
-    const { type } = repoStore.selectedActivity;
+    const { type } = activity;
     const config = repoStore.taxonomy.find((it: any) => it.type === type);
     const configuredSubLevels = get(config, 'subLevels', []);
     return repoStore.taxonomy.filter((it: any) =>
