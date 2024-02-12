c8cf5d061e8cc30b3457826f1ec9529b636f60d8
underscope <underscope@gmail.com>
2024-02-12 14:45:50 +0100

    Migrate ActivityOptions Toolbar to Vue 3

---
+++ apps/frontend-nuxt/components/common/ActivityOptions/Toolbar.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="activity-options-container d-flex">
+    <VTooltip v-for="it in options" :key="it.name" location="bottom">
+      <template #activator="{ props: tooltipProps }">
+        <VBtn
+          v-bind="tooltipProps"
+          :aria-label="it.name"
+          :icon="it.icon"
+          class="mr-1"
+          color="primary-lighten-3"
+          size="small"
+          variant="text"
+          @click.stop="it.action"
+        >
+        </VBtn>
+      </template>
+      <span>{{ it.name }}</span>
+    </VTooltip>
+    <CreateDialog
+      v-if="showCreateDialog"
+      @close="showCreateDialog = false"
+      :repository-id="activity.repositoryId"
+      :anchor="activity"
+      :action="action"
+      :heading="`${selectedActivity?.getAddDialogHeading(action)}: ${
+        activity.data.name
+      }`"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import CreateDialog from '@/components/repository[id]/CreateDialog/index.vue';
+import InsertLocation from '@/lib/InsertLocation';
+import { useSelectedActivity } from '#imports';
+
+const { ADD_AFTER, ADD_BEFORE, ADD_INTO } = InsertLocation;
+
+const props = defineProps({
+  activity: { type: Object, required: true },
+});
+
+const selectedActivity = useSelectedActivity(props.activity);
+const showCreateDialog = ref(false);
+const action = ref('');
+
+const options = computed(() => {
+  const { subLevels, isEditable } = selectedActivity;
+  const items = [
+    {
+      name: 'Add item above',
+      icon: 'add:above',
+      action: () => setCreateContext(ADD_BEFORE),
+    },
+    {
+      name: 'Add item below',
+      icon: 'add:below',
+      action: () => setCreateContext(ADD_AFTER),
+    },
+  ];
+  if (subLevels.value.length) {
+    items.push({
+      name: 'Add item into',
+      icon: 'add:into',
+      action: () => setCreateContext(ADD_INTO),
+    });
+  }
+  if (isEditable.value) {
+    const { id: activityId } = props.activity;
+    items.push({
+      name: 'Open',
+      icon: 'mdi-page-next-outline',
+      action: () => navigateTo({ name: 'editor', params: { activityId } }),
+    });
+  }
+  return items;
+});
+
+const setCreateContext = (actionType: string) => {
+  action.value = actionType;
+  showCreateDialog.value = true;
+};
+</script>
