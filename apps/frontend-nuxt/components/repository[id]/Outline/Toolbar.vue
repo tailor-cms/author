996c3ec7fcb513bd85a7364cc7e79cf58d6e273d
underscope <underscope@gmail.com>
2024-02-13 14:27:23 +0100

    Migrate Outline Toolbar -> Vue 3

---
+++ apps/frontend-nuxt/components/repository[id]/Outline/Toolbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <VToolbar color="transparent"  class="toolbar">
+    <VTextField
+      @input="$emit('search', $event)"
+      @click:clear="$emit('search', '')"
+      :value="search"
+      bg-color="primary-darken-2"
+      prepend-inner-icon="mdi-magnify"
+      placeholder="Search by name or id..."
+      variant="tonal"
+      clearable
+      hide-details
+    />
+    <VSpacer />
+    <VBtn
+      v-if="!isFlat"
+      @click="currentRepositoryStore.toggleOutlineExpand"
+      :disabled="!!props.search"
+      color="primary-lighten-3"
+      variant="text"
+    >
+      Toggle all
+    </VBtn>
+  </VToolbar>
+</template>
+
+<script lang="ts" setup>
+import { useCurrentRepository } from '@/stores/current-repository';
+
+const props = defineProps({
+  search: { type: String, default: '' },
+  isFlat: { type: Boolean, default: false },
+});
+
+const currentRepositoryStore = useCurrentRepository();
+</script>
+
+<style lang="scss" scoped>
+.toolbar {
+  z-index: 1;
+}
+</style>
