f6e5486561d3320e9008af8e932402559494520d
underscope <underscope@gmail.com>
2024-02-13 14:12:24 +0100

    Migrate OutlineFooter to Vue 3

---
+++ apps/frontend-nuxt/components/repository[id]/Outline/OutlineFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <VRow>
+    <VCol class="text-left">
+      <VAlert
+        v-if="!anchor"
+        class="mb-5"
+        color="primary-lighten-3"
+        icon="mdi-information-outline"
+        variant="tonal"
+        prominent
+      >
+        Click on the button below in order to create your first item!
+      </VAlert>
+      <CreateDialog
+        :repository-id="repositoryId"
+        :anchor="anchor"
+        activator-color="primary-lighten-4"
+        test-id-prefix="repository__createRootActivity"
+        show-activator
+      />
+    </VCol>
+  </VRow>
+</template>
+
+<script lang="ts" setup>
+import CreateDialog from '@/components/repository[id]/CreateDialog/index.vue';
+import last from 'lodash/last';
+import { storeToRefs } from 'pinia';
+import { useCurrentRepository } from '@/stores/current-repository';
+
+const store = useCurrentRepository();
+const { repositoryId, rootActivities } = storeToRefs(store);
+const anchor = computed(() => last(rootActivities.value));
+</script>
