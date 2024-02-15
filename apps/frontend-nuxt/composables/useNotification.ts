2101880b1bc42b284a04b94d17c6960ac3f24a24
underscope <underscope@gmail.com>
2024-02-15 11:55:43 +0100

    Add notification snackbar

---
+++ apps/frontend-nuxt/composables/useNotification.ts
@@ -0,0 +1,14 @@
+interface NotificationOptions {
+  color?: string;
+  timeout?: number;
+  immediate?: boolean;
+}
+export const useNotification = () => {
+  const { $eventBus } = useNuxtApp() as any;
+  return (message: string, opts: NotificationOptions) => {
+    $eventBus.channel('app').emit('showNotificationSnackbar', {
+      message,
+      ...opts
+    });
+  };
+};
