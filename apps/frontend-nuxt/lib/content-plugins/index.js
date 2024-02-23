55c83d8e2734fb3fc8447ecb9116534e89111ac5
underscope <underscope@gmail.com>
2024-02-23 13:48:12 +0100

    Expose registries

--- apps/frontend-nuxt/lib/content-plugins/index.js
+++ apps/frontend-nuxt/lib/content-plugins/index.js
@@ -1,7 +1,11 @@
+import ContentContainerRegistry from './ContentContainerRegistry';
+import ContentElementRegistry from './ContentElementRegistry';
 import MetaRegistry from './MetaRegistry';
 
 export default class ContentRepository {
   constructor(app) {
+    this.contentElementRegistry = ContentElementRegistry(app);
+    this.contentContainerRegistry = ContentContainerRegistry(app);
     this.metaRegistry = MetaRegistry(app);
   }
 }
--- apps/frontend-nuxt/plugins/content-plugins.ts
+++ apps/frontend-nuxt/plugins/content-plugins.ts
@@ -3,4 +3,6 @@ import ContentRepository from '@/lib/content-plugins';
 export default defineNuxtPlugin((nuxtApp) => {
   const pluginRegistry = new ContentRepository(nuxtApp.vueApp);
   nuxtApp.provide('pluginRegistry', pluginRegistry);
+  nuxtApp.provide('ccRegistry', pluginRegistry.contentContainerRegistry);
+  nuxtApp.provide('ceRegistry', pluginRegistry.contentElementRegistry);
 });
