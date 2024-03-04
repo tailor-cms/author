9363aa5bae2afa7b6d7a1f5ad64defa2f176135c
underscope <underscope@gmail.com>
2024-03-04 12:38:08 +0100

    Add generation to the page container

---
+++ packages/core-components-next/src/components/CircularProgress.vue
@@ -0,0 +1,18 @@
+<template>
+  <VProgressCircular
+    bg-color="primary"
+    color="primary-darken-4"
+    size="68"
+    indeterminate
+  >
+    <template #default>
+      <img
+        alt="Tailor logo"
+        class="pt-1"
+        height="52"
+        src="/img/default-logo-full.svg"
+        width="32"
+      />
+    </template>
+  </VProgressCircular>
+</template>
