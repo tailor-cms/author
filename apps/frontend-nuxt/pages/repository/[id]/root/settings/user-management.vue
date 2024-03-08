5fc2fc1d3e962b8444a20009a2a4bf10d9765b28
underscope <underscope@gmail.com>
2024-03-08 20:32:35 +0100

    Init repository User Management page

---
+++ apps/frontend-nuxt/pages/repository/[id]/root/settings/user-management.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      <AddUserDialog :roles="roles" />
+    </div>
+    <UserList :roles="roles" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import map from 'lodash/map';
+import { role } from 'tailor-config-shared';
+import { title as titleCase } from 'to-case';
+
+import AddUserDialog from '@/components/repository/Settings/UserManagement/AddUserDialog.vue';
+import UserList from '@/components/repository/Settings/UserManagement/UserList.vue';
+
+definePageMeta({
+  name: 'repository-settings-users',
+});
+
+const roles = computed(() =>
+  map(role.repository, (value) => ({ title: titleCase(value), value })),
+);
+</script>
