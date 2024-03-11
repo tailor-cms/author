2791dfb1b6af2bf0298a3119ac4749ba98a714d3
underscope <underscope@gmail.com>
2024-03-11 13:04:16 +0100

    Migrate revision list essentials

---
+++ apps/frontend-nuxt/api/interfaces/revision.ts
@@ -0,0 +1,40 @@
+export interface Revision {
+  id: number;
+  uid: string;
+  repositoryId: number;
+  userId: number;
+  entity: string;
+  operation: string;
+  state: State;
+  user: User;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface State {
+  id: number;
+  uid: string;
+  activityId: number;
+  data: any;
+  meta: Record<string, unknown>;
+  refs: Record<string, unknown>;
+  type: string;
+  linked: boolean;
+  detached: boolean;
+  position: number;
+  contentId: string;
+  repositoryId: number;
+  contentSignature: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: null | string;
+}
+
+interface User {
+  id: number;
+  email: string;
+  firstName: null | string;
+  fullName: null | string;
+  lastName: null | string;
+  label: string;
+}
---
+++ apps/frontend-nuxt/components/repository/Revisions/RevisionItem.vue
@@ -0,0 +1,97 @@
+<template>
+  <li>
+    <div
+      :class="{ isExpanded, expandable: false && isContentElement }"
+      :style="{ cursor: false && isContentElement ? 'pointer' : 'auto' }"
+      class="revision"
+      @click="toggle"
+    >
+      <VAvatar :color="color" size="42">{{ acronym }}</VAvatar>
+      <div class="content ml-3">
+        <div class="text-subtitle-1 text-truncate text-primary-lighten-4">
+          {{ description }}
+        </div>
+        <div class="text-body-1 text-primary-lighten-2">
+          {{ date }} {{ revision.user.label }}
+        </div>
+      </div>
+    </div>
+    <!-- <EntityRevisions
+      v-if="isExpanded"
+      v-bind="{ revision: props.revision, isDetached: !activity }"
+    /> -->
+  </li>
+</template>
+
+<script lang="ts" setup>
+import {
+  getFormatDescription,
+  getRevisionAcronym,
+  getRevisionColor,
+} from '@/lib/revision';
+import fecha from 'fecha';
+import find from 'lodash/find';
+import { useActivityStore } from '@/stores/activity';
+import { useCurrentRepository } from '@/stores/current-repository';
+
+// TODO: Needs to be migrated
+// import EntityRevisions from './EntityRevisions.vue';
+
+const activityStore = useActivityStore();
+const currentRepositoryStore = useCurrentRepository();
+
+const props = defineProps<{ revision: Record<string, any> }>();
+
+const isExpanded = ref(false);
+
+const activity = computed(() => {
+  const { state } = props.revision;
+  const activityId = state.activityId || state.id;
+  return getOutlineLocation(activityStore.getParent(activityId));
+});
+
+const color = computed(() => getRevisionColor(props.revision));
+const acronym = computed(() => getRevisionAcronym(props.revision));
+
+const date = computed(() =>
+  fecha.format(new Date(props.revision.createdAt), 'M/D/YY h:mm A'),
+);
+
+const description = computed(() =>
+  getFormatDescription(props.revision, activity.value),
+);
+
+const isContentElement = computed(
+  () => props.revision.entity === 'CONTENT_ELEMENT',
+);
+
+const getOutlineLocation: any = (activity: any) => {
+  if (!activity) return null;
+  const level = find(currentRepositoryStore.taxonomy, { type: activity.type });
+  if (level) return { ...activity, label: level.label };
+  return getOutlineLocation(activityStore.getParent(activity.id));
+};
+
+const toggle = () => {
+  if (isContentElement.value) isExpanded.value = !isExpanded.value;
+};
+</script>
+
+<style lang="scss" scoped>
+.revision {
+  display: flex;
+  align-items: center;
+  min-height: 4.5rem;
+  padding: 0 1rem;
+
+  .content {
+    flex: 1;
+    overflow: hidden;
+  }
+}
+
+.expandable.isExpanded,
+.expandable.revision:hover {
+  background-color: #dadada;
+}
+</style>
