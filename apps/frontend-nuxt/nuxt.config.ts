6120d4d5ebea2ca6d7b8d8e0559acb0261aae7e4
underscope <underscope@gmail.com>
2024-03-13 07:25:08 +0100

    Use nuxt frontend upon serving the built version

--- apps/frontend-nuxt/nuxt.config.ts
+++ apps/frontend-nuxt/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  router: {
+    options: { hashMode: true },
+  },
   debug: true,
   modules: ['@pinia/nuxt'],
   build: {
