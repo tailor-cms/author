8a5e1a86565f7267ab5b30067dd975ee37283448
underscope <underscope@gmail.com>
2024-03-22 07:34:10 +0100

    Migrate Element Sidebar

---
+++ apps/frontend-nuxt/components/editor/Sidebar/ElementSidebar/ElementMeta/Inputs.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="meta-inputs">
+    <MetaInput
+      v-for="input in inputs"
+      :key="`${id}.${input.key}`"
+      :meta="input"
+      @update="updateElement"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import MetaInput from '@/components/common/MetaInput.vue';
+
+const props = defineProps({
+  element: { type: Object, required: true },
+  inputs: { type: Array, default: () => [] },
+});
+
+const elementBus = inject('$elementBus') as any;
+const id = computed(() => getElementId(props.element));
+
+const updateElement = (key, value) => {
+  const meta = { ...props.element.meta };
+  meta[key] = value;
+  elementBus.emit('save:meta', meta);
+};
+</script>
---
+++ apps/frontend-nuxt/components/editor/Sidebar/ElementSidebar/ElementMeta/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="element-metadata">
+    <ElementInputs
+      v-if="inputs.length"
+      :element="props.element"
+      :inputs="props.inputs"
+    />
+    <!-- TODO: Needs to be implemented once element selection is implemented
+    <ElementRelationships
+      v-if="props.relationships.length && props.element.activityId"
+      :element="props.element"
+      :relationships="props.relationships"
+    />-->
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ElementInputs from './Inputs.vue';
+// import ElementRelationships from './Relationships/index.vue';
+
+const props = defineProps({
+  element: { type: Object, required: true },
+  inputs: { type: Array, default: () => [] },
+  relationships: { type: Array, default: () => [] },
+});
+</script>
