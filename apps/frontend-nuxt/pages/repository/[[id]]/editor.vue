c3b09283abf295c3d6424e4d2b441b2ccdc4d107
underscope <underscope@gmail.com>
2024-03-25 08:42:34 +0100

    Move sidebar and toolbar to editor root

--- apps/frontend-nuxt/pages/repository/[[id]]/editor.vue
+++ apps/frontend-nuxt/pages/repository/[[id]]/editor.vue
@@ -1,16 +1,73 @@
 <template>
-  <NuxtPage v-if="activityId" :key="activityId" :activityId="activityId" />
+  <div>
+    <VToolbar
+      v-if="editorStore.selectedActivity"
+      :key="`${editorStore.selectedActivityId}-${editorStore.selectedContentElementId}`"
+      :element="editorStore.selectedContentElement as ContentElement"
+      :active-users="activeUsers"
+    />
+    <div class="editor-content-container">
+      <VSidebar
+        :repository="repositoryStore.repository as Repository"
+        :activities="repositoryStore.outlineActivities as Activity[]"
+        :selected-activity="editorStore.selectedActivity as Activity"
+        :selected-element="editorStore.selectedContentElement as ContentElement"
+        class="sidebar"
+      />
+      <NuxtPage
+        v-if="activityId"
+        :key="activityId"
+        :activityId="activityId"
+        class="activity-content"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import type { Activity } from '@/api/interfaces/activity';
+import type { ContentElement } from '@/api/interfaces/content-element';
+import type { Repository } from '@/api/interfaces/repository';
+import { useCurrentRepository } from '@/stores/current-repository';
+import { useEditorStore } from '@/stores/editor';
+import VSidebar from '@/components/editor/Sidebar/index.vue';
+import VToolbar from '@/components/editor/Toolbar/index.vue';
+
+const repositoryStore = useCurrentRepository();
+const editorStore = useEditorStore();
 const route = useRoute();
+
 const activityId = ref<number | null>(null);
+// TODO: Needs to be implemented
+const activeUsers: any = [];
 
 const parseActivityId = () => {
   if (!route.params.activityId) navigateTo({ name: 'catalog' });
   activityId.value = parseInt(route.params.activityId as string, 10);
 };
 
-onBeforeMount(() => parseActivityId());
+onBeforeMount(() => {
+  parseActivityId();
+  editorStore.initialize(activityId.value as number);
+});
+
 watch(() => route.params?.activityId, parseActivityId);
 </script>
+
+<style lang="scss" scoped>
+$sidebar-width: 25rem;
+
+.editor-content-container {
+  display: flex;
+  height: calc(100% - 3.5rem);
+
+  .sidebar {
+    flex-basis: $sidebar-width;
+  }
+
+  .activity-content {
+    flex-grow: 1;
+    flex-basis: calc(100% - #{$sidebar-width});
+  }
+}
+</style>
--- apps/frontend-nuxt/pages/repository/[[id]]/editor/[activityId].vue
+++ apps/frontend-nuxt/pages/repository/[[id]]/editor/[activityId].vue
@@ -1,44 +1,23 @@
 <template>
-  <div>
-    <VToolbar
+  <div class="d-flex flex-column w-100">
+    <VActivityContent
       v-if="editorStore.selectedActivity"
-      :key="editorStore.selectedContentElementId as number"
-      :element="editorStore.selectedContentElement as ContentElement"
-      :active-users="activeUsers"
+      :key="editorStore.selectedActivity.id"
+      :repository="repositoryStore.repository"
+      :activity="editorStore.selectedActivity"
+      :content-containers="editorStore.contentContainers"
+      :root-container-groups="editorStore.rootContainerGroups"
+      @selected="selectElement"
     />
-    <div class="editor-content-container">
-      <VSidebar
-        :repository="repositoryStore.repository as Repository"
-        :activities="repositoryStore.outlineActivities as Activity[]"
-        :selected-activity="editorStore.selectedActivity as Activity"
-        :selected-element="editorStore.selectedContentElement as ContentElement"
-        class="sidebar"
-      />
-      <VActivityContent
-        v-if="editorStore.selectedActivity"
-        :key="editorStore.selectedActivity.id"
-        :repository="repositoryStore.repository as Repository"
-        :activity="editorStore.selectedActivity"
-        :content-containers="editorStore.contentContainers"
-        :root-container-groups="editorStore.rootContainerGroups"
-        class="activity-content"
-        @selected="selectElement"
-      />
-    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { getElementId } from '@tailor-cms/utils';
 
-import type { Activity } from '@/api/interfaces/activity';
-import type { ContentElement } from '@/api/interfaces/content-element';
-import type { Repository } from '@/api/interfaces/repository';
 import { useCurrentRepository } from '@/stores/current-repository';
 import { useEditorStore } from '@/stores/editor';
 import VActivityContent from '@/components/editor/ActivityContent/index.vue';
-import VSidebar from '@/components/editor/Sidebar/index.vue';
-import VToolbar from '@/components/editor/Toolbar/index.vue';
 
 definePageMeta({
   name: 'editor',
@@ -66,8 +45,6 @@ onBeforeMount(() => {
   editorStore.initialize(props.activityId);
 });
 
-// TODO: Needs to be implemented
-const activeUsers: any = [];
 // TODO: Publish diff, Toolbar and Sidebar need to be migrated
 // import VSidebar from './VSidebar/index.vue';
 // const showPublishDiff = computed(() => store.state.editor.showPublishDiff);
@@ -86,21 +63,3 @@ const activeUsers: any = [];
 //   },
 // );
 </script>
-
-<style lang="scss" scoped>
-$sidebar-width: 25rem;
-
-.editor-content-container {
-  display: flex;
-  height: calc(100% - 3.5rem);
-
-  .sidebar {
-    flex-basis: $sidebar-width;
-  }
-
-  .activity-content {
-    flex-grow: 1;
-    flex-basis: calc(100% - #{$sidebar-width});
-  }
-}
-</style>
