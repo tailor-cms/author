4191b8c66bbfa0d479b2b05f09ec70f1aee89d2e
underscope <underscope@gmail.com>
2024-04-11 13:50:44 +0200

    Update baseURL

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   reporter: 'html',
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: 'http://localhost:8081',
+    baseURL: 'http://localhost:8080',
     trace: 'on-first-retry',
   },
   projects: [
