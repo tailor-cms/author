4bc555971a3ff79211026dc9d51dea29fe41db50
underscope <underscope@gmail.com>
2024-04-11 05:21:28 +0200

    Tweak reset-password UI

--- apps/frontend-nuxt/pages/auth/reset-password/[token].vue
+++ apps/frontend-nuxt/pages/auth/reset-password/[token].vue
@@ -1,23 +1,31 @@
 <template>
   <NuxtLayout name="auth">
     <VAlert
-      v-if="message"
-      :color="isError ? 'pink-lighten-1' : 'success'"
-      class="mb-8"
+      v-if="notificationText"
+      :color="isError ? 'pink-lighten-4' : 'teal-lighten-4'"
+      class="mb-8 text-subtitle-2"
       variant="tonal"
     >
-      {{ message }}
+      {{ notificationText }}
     </VAlert>
-    <div v-if="isLoading" class="d-flex justify-center">
-      <VProgressCircular color="primary-darken-2" indeterminate />
+    <div v-if="isLoading" class="d-flex justify-center pt-14 pb-16">
+      <VProgressCircular color="primary-lighten-1" size="56" indeterminate />
     </div>
     <div v-else-if="isError" class="d-flex flex-column justify-center">
-      <NuxtLink :to="{ path: '/auth/forgot-password' }" class="text-center">
+      <NuxtLink
+        :to="{ name: 'forgot-password' }"
+        class="text-center text-primary-lighten-5"
+      >
         <VIcon size="20">mdi-arrow-top-right-thick</VIcon>
         Click here to send another reset email.
       </NuxtLink>
-      <VBtn class="mt-7" to="/auth" variant="tonal">
-        <VIcon class="pr-2">mdi-arrow-left</VIcon>Back to login page
+      <VBtn
+        :to="{ name: 'sign-in' }"
+        class="mt-7"
+        color="primary-lighten-2"
+        variant="tonal"
+      >
+        <VIcon class="pr-2">mdi-arrow-left</VIcon>Sign in
       </VBtn>
     </div>
     <form v-else novalidate @submit.prevent="submit">
@@ -44,7 +52,7 @@
         variant="outlined"
       />
       <VBtn
-        color="primary-darken-4"
+        color="primary-lighten-2"
         type="submit"
         variant="tonal"
         block
@@ -58,11 +66,20 @@
 
 <script lang="ts" setup>
 import { object, string, ref as yupRef } from 'yup';
-import Promise from 'bluebird';
 import { useForm } from 'vee-validate';
 
 import { auth as api } from '@/api';
 
+definePageMeta({
+  name: 'reset-password',
+});
+
+useHead({
+  title: 'Reset password',
+  meta: [{ name: 'description', content: 'Tailor CMS - Reset password page' }],
+});
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 const ERRORS = {
   default: 'An error has occurred!',
   resetToken: 'Invalid reset password token!',
@@ -70,9 +87,10 @@ const ERRORS = {
 
 const route = useRoute();
 const authStore = useAuthStore();
+
 const isLoading = ref(true);
 const isError = ref(false);
-const message = ref('');
+const notificationText = ref('');
 
 const { defineField, errors, handleSubmit } = useForm({
   validationSchema: object({
@@ -93,30 +111,25 @@ const submit = handleSubmit(async ({ password }) => {
       token: route.params.token as string,
     });
     isError.value = false;
-    message.value = 'Password changed successfully. Redirecting...';
-    await Promise.delay(2000);
-    navigateTo('/');
+    notificationText.value = 'Password changed successfully. Redirecting...';
+    await delay(3000);
+    navigateTo({ name: 'sign-in' });
   } catch {
     isError.value = true;
-    message.value = ERRORS.default;
+    notificationText.value = ERRORS.default;
   }
 });
 
 onMounted(async () => {
+  // Make sure the loader is visible for at least 1 second
+  await delay(1000);
   try {
     await api.validateResetToken(route.params.token);
   } catch {
     isError.value = true;
-    message.value = ERRORS.resetToken;
+    notificationText.value = ERRORS.resetToken;
   } finally {
     isLoading.value = false;
   }
 });
 </script>
-
-<style lang="scss" scoped>
-.v-input ::v-deep label {
-  padding-right: 0.25rem;
-  background: #ececec;
-}
-</style>
