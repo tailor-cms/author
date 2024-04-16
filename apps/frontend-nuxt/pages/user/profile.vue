86b2f7e2a658a44cc56d4da180de310f25e55471
underscope <underscope@gmail.com>
2024-04-16 08:13:44 +0200

    Init user-profile page

---
+++ apps/frontend-nuxt/pages/user/profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtLayout name="main">
+    <VContainer>
+      <VRow>
+        <VCol>
+          <VAlert color="primary-lighten-3" variant="tonal">
+            Hello, User!
+          </VAlert>
+        </VCol>
+      </VRow>
+    </VContainer>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  name: 'user-profile',
+  middleware: ['auth'],
+});
+</script>
