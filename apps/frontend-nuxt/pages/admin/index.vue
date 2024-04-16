47cab03a419c9784665afc977a29dc6f0d7564bc
underscope <underscope@gmail.com>
2024-04-16 08:00:24 +0200

    Init admin page

---
+++ apps/frontend-nuxt/pages/admin/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLayout name="main">
+    <VContainer>
+      <VRow>
+        <VCol>
+          <VAlert color="primary-lighten-3" variant="tonal">
+            Hello, Admin!
+          </VAlert>
+        </VCol>
+      </VRow>
+    </VContainer>
+  </NuxtLayout>
+</template>
