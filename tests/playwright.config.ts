be294032d63a1ba8a489add120352521f849f0a5
underscope <underscope@gmail.com>
2024-04-19 15:40:14 +0200

    Update worker config

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -6,11 +6,10 @@ dotenv.config();
 // See https://playwright.dev/docs/test-configuration.
 export default defineConfig({
   testDir: './specs',
-  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   // Opt out of parallel tests on CI.
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   reporter: 'html',
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
