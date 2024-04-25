e9b9f33401e39b7df6aeb794f760a3607333b44d
underscope <underscope@gmail.com>
2024-04-25 16:59:49 +0200

    💄

--- apps/frontend/components/repository/Outline/OutlineFooter.vue
+++ apps/frontend/components/repository/Outline/OutlineFooter.vue
@@ -14,7 +14,7 @@
       <CreateDialog
         :anchor="anchor"
         :repository-id="repositoryId as number"
-        activator-color="primary-lighten-4"
+        activator-color="primary-lighten-2"
         test-id-prefix="repository__createRootActivity"
         show-activator
       />
