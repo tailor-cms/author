cbe612d0d7c5b4841434c0ef7a8373197d0e886e
underscope <underscope@gmail.com>
2024-04-25 16:58:12 +0200

    Tweak notification snackbar UI

--- apps/frontend/composables/useNotification.ts
+++ apps/frontend/composables/useNotification.ts
@@ -5,8 +5,8 @@ interface NotificationOptions {
 }
 export const useNotification = () => {
   const { $eventBus } = useNuxtApp() as any;
-  return (message: string, opts?: NotificationOptions) => {
-    opts = opts || {};
+  return (message: string, opts: NotificationOptions = {}) => {
+    if (!opts.color) opts.color = 'primary-darken-3';
     $eventBus.channel('app').emit('showNotificationSnackbar', {
       message,
       ...opts,
