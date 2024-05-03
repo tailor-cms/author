ef970893ac4af5e2d7e2bd0c13ac271ce4785faf
underscope <underscope@gmail.com>
2024-05-03 20:45:12 +0200

    Init resource provisioning

--- infrastructure/env.ts
+++ infrastructure/env.ts
@@ -1,25 +1,27 @@
 import * as pulumi from '@pulumi/pulumi';
 
-const config = new pulumi.Config('aws');
+const config = new pulumi.Config();
+const awsConfig = new pulumi.Config('aws');
+const ssmConfig = new pulumi.Config('ssm');
 
-const getSecret(key: string) {
-  const region = config.require('region');
-  const accountId = config.require('accountId');
-  const ssmNamePrefix = config.require('ssmNamePrefix');
-  return `arn:aws:ssm:${region}:${accountId}:parameter/${ssmNamePrefix}/${key}`;
+function getSecret(key: string) {
+  const accountId = config.getSecret('accountId');
+  const region = awsConfig.require('region');
+  const ssmKeyPrefix = ssmConfig.require('keyPrefix');
+  return pulumi.interpolate`arn:aws:ssm:${region}:${accountId}:parameter/${ssmKeyPrefix}/${key}`;
 }
 
 const serverSecrets = [
   'AUTH_JWT_SECRET',
   'AUTH_JWT_COOKIE_SECRET',
-  'AI_SECRET_KEY',
   'STORAGE_KEY',
   'STORAGE_SECRET',
   'EMAIL_PASSWORD',
   'EMAIL_USER',
+  'AI_SECRET_KEY',
 ];
 
-export const secrets = serverSecrets.map(it => ({
+export const secrets = serverSecrets.map((it) => ({
   name: it,
   valueFrom: getSecret(it),
 }));
--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,58 +1,90 @@
+import * as aws from '@pulumi/aws';
+import * as awsx from '@pulumi/awsx';
 import * as pulumi from '@pulumi/pulumi';
 import * as studion from '@studion/infra-code-blocks';
 
 import { secrets } from './env';
 
 const config = new pulumi.Config();
-const awsConfig = new pulumi.Config('aws');
+const dnsConfig = new pulumi.Config('dns');
 
-const org = config.require('pulumi-org');
-const name = pulumi.getProject();
-const stack = pulumi.getStack();
-const domain = config.require('baseDomain');
-const hostedZoneId = awsConfig.require('hostedZoneId');
-const namePrefix = awsConfig.require('namePrefix');
-const isProduction = stack === 'prod';
+const PROJECT_NAME = pulumi.getProject();
+const STACK = pulumi.getStack();
+
+const resourceNamePrefix = config.require('resourceNamePrefix');
+const fullPrefix = `${resourceNamePrefix}-${STACK}`;
 
 export const serverImage = process.env.SERVER_IMAGE;
 if (!serverImage) throw new Error('Server image env is required');
 
-// Self reference to get outputs from the stack created by the
-// higher level abstractions like studion.Database
-const stackRef = new pulumi.StackReference(`${org}/${name}/${stack}`);
-const db = new studion.Database(`${namePrefix}-tailor-cms-db-${stack}`, {
-  vpcId: stackRef.getOutput('vpcId'),
-  isolatedSubnetIds: stackRef.getOutput('isolatedSubnetIds'),
-  vpcCidrBlock: stackRef.getOutput('vpcCidrBlock'),
-  dbName: 'tailor_cms',
-  username: 'tailor_cms',
-  instanceClass: 'db.t4g.micro',
+const vpc = new awsx.ec2.Vpc(`${PROJECT_NAME}-vpc`, {
+  enableDnsHostnames: true,
+  numberOfAvailabilityZones: 2,
+  natGateways: { strategy: 'None' },
+  subnetStrategy: 'Auto',
+  subnetSpecs: [
+    { type: awsx.ec2.SubnetType.Public, cidrMask: 24 },
+    { type: awsx.ec2.SubnetType.Private, cidrMask: 24 },
+    { type: awsx.ec2.SubnetType.Isolated, cidrMask: 24 },
+  ],
 });
 
-const webServer = new studion.WebServer(`${namePrefix}-tailor-cms-${stack}`, {
+const db = new studion.Database(`${fullPrefix}-tailor-db`, {
+  instanceClass: 'db.t4g.micro',
+  dbName: 'tailor_cms',
+  username: 'tailor_cms',
+  vpcId: vpc.vpcId,
+  vpcCidrBlock: vpc.vpc.cidrBlock,
+  isolatedSubnetIds: vpc.publicSubnetIds,
+});
+
+const cluster = new aws.ecs.Cluster(`${fullPrefix}-ecs-cluster`, {
+  name: `${fullPrefix}-ecs-cluster`,
+});
+
+const webServer = new studion.WebServer(`${fullPrefix}-server`, {
   image: serverImage,
   port: 3000,
-  domain,
-  vpcId: stackRef.getOutput('vpcId'),
-  vpcCidrBlock: stackRef.getOutput('vpcCidrBlock'),
-  publicSubnetIds: stackRef.getOutput('publicSubnetIds'),
-  clusterId: stackRef.getOutput('clusterId'),
-  clusterName: stackRef.getOutput('clusterName'),
-  hostedZoneId,
+  domain: dnsConfig.require('domain'),
+  vpcId: vpc.vpcId,
+  vpcCidrBlock: vpc.vpc.cidrBlock,
+  publicSubnetIds: vpc.publicSubnetIds,
+  clusterId: cluster.id,
+  clusterName: cluster.name,
+  hostedZoneId: dnsConfig.require('hostedZoneId'),
   autoscaling: { enabled: false },
-  size: isProduction ? 'medium' : 'small',
+  size: 'small',
   desiredCount: 1,
   healthCheckPath: '/healthcheck',
   environment: [
+    { name: 'NODE_ENV', value: 'development' },
+    { name: 'HOSTNAME', value: 'tailor-cms.com' },
+    { name: 'PROTOCOL', value: 'https' },
     { name: 'PORT', value: '3000' },
-    { name: 'STORAGE_BUCKET', value: stackRef.getOutput('bucketName') },
-    { name: 'DATABASE_NAME', value: db.instance.dbName },
+    { name: 'REVERSE_PROXY_PORT', value: '443' },
+    { name: 'CORS_ALLOWED_ORIGINS', value: 'https://tailor-cms.com' },
     { name: 'DATABASE_USER', value: db.instance.username },
+    { name: 'DATABASE_NAME', value: db.instance.dbName },
     { name: 'DATABASE_HOST', value: db.instance.address },
     {
       name: 'DATABASE_PORT',
-      value: db.instance.port.apply((port) => String(port)),
+      value: db.instance.port.apply((port: number) => String(port)),
     },
+    { name: 'DATABASE_ADAPTER', value: 'postgres' },
+    { name: 'DATABASE_SSL', value: 'true' },
+    { name: 'STORAGE_PROVIDER', value: 'amazon' },
+    { name: 'STORAGE_REGION', value: 'eu-central-1' },
+    { name: 'STORAGE_BUCKET', value: 'tailor-cms-dev' },
+    { name: 'LOG_LEVEL', value: 'DEBUG' },
+    { name: 'AUTH_JWT_ISSUER', value: 'tailor' },
+    { name: 'AUTH_JWT_COOKIE_NAME', value: 'access_token' },
+    { name: 'AUTH_SALT_ROUNDS', value: '10' },
+    { name: 'NUXT_PUBLIC_AI_UI_ENABLED', value: 'true' },
+    { name: 'EMAIL_SENDER_NAME', value: 'Tailor' },
+    { name: 'EMAIL_HOST', value: 'email-smtp.us-east-1.amazonaws.com' },
+    { name: 'EMAIL_SSL', value: '1' },
+    { name: 'AI_MODEL_ID', value: 'gpt-4-0125-preview' },
+    { name: 'FLAT_REPO_STRUCTURE', value: '1' },
   ],
   secrets: [
     ...secrets,
