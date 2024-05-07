481bb47bb439c636cf619a687b289ef5608eb729
underscope <underscope@gmail.com>
2024-05-08 00:34:51 +0200

    💄

--- apps/frontend/middleware/auth.ts
+++ apps/frontend/middleware/auth.ts
@@ -2,7 +2,7 @@ import { useAuthStore } from '@/stores/auth';
 
 export default async function () {
   const isAuthenticated = useCookie('is-authenticated');
-  if (!isAuthenticated.value) navigateTo({ name: 'sign-in' });
+  if (!isAuthenticated.value) return navigateTo({ name: 'sign-in' });
   const authStore = useAuthStore();
   if (authStore.user) return;
   await authStore.fetchUserInfo();
