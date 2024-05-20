1476af744197fa6ab742db46a16327d0c2bb93cd
underscope <underscope@gmail.com>
2024-05-20 14:31:56 +0200

    Use same repository name for visual snapshots

--- tests/specs/visual/repository.spec.ts
+++ tests/specs/visual/repository.spec.ts
@@ -1,23 +1,24 @@
 import { expect, test } from '@playwright/test';
 
+import { EndpointClient, getEndpointClient } from '../../api/client';
 import { AddRepositoryDialog } from '../../pom/catalog/AddRepository';
 import { Catalog } from '../../pom/catalog/Catalog';
 import { percySnapshot } from '../../utils/percy.ts';
 
 const TEST_REPOSITORY_NAME = 'Visual test imported repository';
+let REPOSITORY_API: EndpointClient;
 
-test.beforeAll(async ({ page }) => {
-  const catalog = new Catalog(page);
-  await catalog.visit();
-  const dialog = new AddRepositoryDialog(page);
-  await dialog.open();
-  await dialog.importRepository(TEST_REPOSITORY_NAME, 'Test description');
-  await page.reload();
+test.beforeAll(async ({ baseURL }) => {
+  if (!baseURL) throw new Error('baseURL is required');
+  REPOSITORY_API = await getEndpointClient(baseURL, '/api/repositories/');
 });
 
 test.beforeEach(async ({ page }) => {
   const catalog = new Catalog(page);
   await catalog.visit();
+  const dialog = new AddRepositoryDialog(page);
+  await dialog.open();
+  await dialog.importRepository(TEST_REPOSITORY_NAME, 'Test description');
   await expect(page.getByText(TEST_REPOSITORY_NAME)).toBeVisible({
     timeout: 10000,
   });
@@ -34,4 +35,13 @@ test('Take a snapshot of the settings page', async ({ page }) => {
   await tabNavigation.getByText('Settings').click();
   await page.waitForLoadState('networkidle');
   await percySnapshot(page, 'Repository settings page');
+});
+
+test.afterEach(async () => {
+  const { data } = await REPOSITORY_API.list();
+  const { items: repositories } = data;
+  if (!repositories.length) return
+  for (const repository of repositories) {
+    await REPOSITORY_API.remove(repository.id);
+  }
 });
