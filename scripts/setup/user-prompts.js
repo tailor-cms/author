7ff7c3a2e843c87e4ee52b38a19b2064ab3997c9
underscope <underscope@gmail.com>
2024-05-24 06:08:07 +0200

    Add user prompts to support dev setup

---
+++ scripts/setup/user-prompts.js
@@ -0,0 +1,33 @@
+import select from '@inquirer/select';
+
+export function isUsingComposeSpec() {
+  return select({
+    message: 'Select the database setup method:',
+    choices: [
+      {
+        name: 'Use Docker to provide DB service',
+        value: true,
+      },
+      {
+        name: 'I have my own setup',
+        value: false,
+      },
+    ],
+  });
+}
+
+export function shouldSeedTheDatabase() {
+  return select({
+    message: 'Seed the database?',
+    choices: [
+      {
+        name: 'Yes',
+        value: true,
+      },
+      {
+        name: 'No',
+        value: false,
+      },
+    ],
+  });
+}
