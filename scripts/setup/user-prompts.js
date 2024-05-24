ff01ef845970fe4070f60cb01673b048bd793ed1
underscope <underscope@gmail.com>
2024-05-24 10:31:36 +0200

    🔧 Update the condition

--- scripts/setup/user-prompts.js
+++ scripts/setup/user-prompts.js
@@ -3,14 +3,14 @@ import select from '@inquirer/select';
 
 export async function shouldUseComposeSpec() {
   const dbPid = await portToPid(5432).catch(() => null);
-  const portActiveMsg = !dbPid ? 'Port 5432 already in use!' : '';
+  const portActiveMsg = dbPid ? 'Port 5432 already in use!' : '';
   return select({
     message: 'Select the database setup method:',
     choices: [
       {
         name: `Configure Docker to provide DB service. ${portActiveMsg}`,
         value: true,
-        disabled: !dbPid,
+        disabled: !!dbPid,
       },
       {
         name: 'I already have a DB service running',
