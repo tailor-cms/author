b3eb8eaffab2850f4b308b01f31521db68c61197
underscope <underscope@gmail.com>
2024-06-12 10:04:31 +0200

    Init ContainerList pom

--- tests/pom/editor/Editor.ts
+++ tests/pom/editor/Editor.ts
@@ -1,5 +1,6 @@
 import { expect, Locator, Page } from '@playwright/test';
 
+import { ContainerList } from './ContainerList';
 import { EditorSidebar } from './Sidebar';
 
 export class Editor {
@@ -9,11 +10,13 @@ export class Editor {
   readonly primaryPageName = 'History of Pizza';
   readonly primaryPageContent = 'The story of pizza begins';
   readonly secondaryPageName = 'Different Pizza Styles Around the World';
+  readonly containerList: ContainerList;
 
   constructor(page: Page) {
     this.page = page;
     this.sidebar = new EditorSidebar(page);
     this.topToolbar = this.page.locator('.activity-toolbar');
+    this.containerList = new ContainerList(page);
   }
 
   async toPrimaryPage() {
