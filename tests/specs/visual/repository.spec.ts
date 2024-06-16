e5670ec26c17453c03b1a0384ae713c9009a0477
underscope <underscope@gmail.com>
2024-06-16 12:39:56 +0200

    Add history page snapshot

--- tests/specs/visual/repository.spec.ts
+++ tests/specs/visual/repository.spec.ts
@@ -1,22 +1,51 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
+import { ActivityOutline } from '../../pom/repository/Outline.ts';
+import ApiClient from '../../api/ApiClient';
 import { percySnapshot } from '../../utils/percy.ts';
 import SeedClient from '../../api/SeedClient';
 
-test.beforeEach(async ({ page }) => {
+const REPOSITORY_API = new ApiClient('/api/repositories/');
+
+const toSeededRepository = async (page) => {
   await SeedClient.resetDatabase();
   const { data } = await SeedClient.seedTestRepository({
     name: 'Visual test imported repository',
   });
   await page.goto(`/repository/${data.repository.id}/root/structure`);
-});
+};
+
+const toEmptyRepository = async (page) => {
+  const payload = {
+    schema: 'COURSE_SCHEMA',
+    name: 'Visual test imported repository',
+    description: 'Test',
+  };
+  const { data: repository } = await REPOSITORY_API.create(payload as any);
+  await page.goto(`/repository/${repository.id}/root/structure`);
+  await page.waitForLoadState('networkidle');
+  return repository;
+};
 
 test('Take a snapshot of the repository structure page', async ({ page }) => {
+  await toSeededRepository(page);
   await page.getByText('Introduction to Pizza Making').isVisible();
   await percySnapshot(page, 'Repository structure page');
 });
 
+test('Take a snapshot of the history page', async ({ page }) => {
+  await toEmptyRepository(page);
+  const outline = new ActivityOutline(page);
+  await outline.addRootItem('Module', 'Module 1');
+  const tabNavigation = page.getByTestId('repositoryRoot_nav');
+  await tabNavigation.getByText('History').click();
+  await expect(page.getByText('Created repository')).toBeVisible();
+  await expect(page.getByText('Created Module 1 module')).toBeVisible();
+  await percySnapshot(page, 'Repository history page');
+});
+
 test('Take a snapshot of the settings page', async ({ page }) => {
+  await toSeededRepository(page);
   const tabNavigation = page.getByTestId('repositoryRoot_nav');
   await tabNavigation.getByText('Settings').click();
   await page.waitForLoadState('networkidle');
