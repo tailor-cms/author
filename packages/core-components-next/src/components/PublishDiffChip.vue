d6d27807d86e7633e49deeec830c93475f0ee231
rkusan00 <renato.kusanovic@gmail.com>
2024-06-19 16:36:17 +0200

    Migrate chip

---
+++ packages/core-components-next/src/components/PublishDiffChip.vue
@@ -0,0 +1,20 @@
+<template>
+  <VChip
+    v-if="changeType"
+    :color="changeType === 'new' ? 'success' : 'secondary'"
+    class="readonly font-weight-medium text-capitalize"
+    size="small"
+    round
+  >
+    {{ changeType }}
+  </VChip>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+// TODO: Update the type once utils is migrated to ts
+withDefaults(defineProps<{ changeType?: string | null }>(), {
+  changeType: null,
+});
+</script>
