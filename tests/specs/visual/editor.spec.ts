e74621e4ffa0f2fe4604a728547749e4e4f71b35
underscope <underscope@gmail.com>
2024-06-26 15:47:11 +0200

    Add visual test for HTML element editing

--- tests/specs/visual/editor.spec.ts
+++ tests/specs/visual/editor.spec.ts
@@ -1,5 +1,6 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
+import { Editor } from '../../pom/editor/Editor';
 import { percySnapshot } from '../../utils/percy.ts';
 import SeedClient from '../../api/SeedClient';
 
@@ -18,6 +19,21 @@ test('Take a snapshot of the editor page', async ({ page }) => {
   await page.getByText('The story of pizza begins').isVisible();
   await page.waitForTimeout(2000);
   await percySnapshot(page, 'Editor page');
+});
+
+test('Take a snapshot of the editor page upon editing the HTML CE', async ({
+  page,
+}) => {
+  await page.getByText('The story of pizza begins').isVisible();
+  await page.waitForTimeout(2000);
+  const editor = new Editor(page);
+  await expect(page.getByText(editor.primaryPageContent)).toBeVisible();
+  const containers = await editor.containerList.getContainers();
+  const elements = await containers[0].getElements();
+  await elements[0].el.click();
+  // Make sure the sidebar is visible
+  await expect(page.getByText('Additional settings')).toBeVisible();
+  await percySnapshot(page, 'Editor page - HTML Content Element editing');
 });
 
 test.afterAll(async () => {
