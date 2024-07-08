040bb865f90446ac4b1eebd54d8ce4bec5db98a8
rkusan00 <renato.kusanovic@gmail.com>
2024-07-08 21:03:58 +0200

    Migrate meta-html

---
+++ packages/core-extensions/meta-inputs/meta-html/src/EditMeta/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <RichTextEditor v-model="input" />
+</template>
+
+<script lang="ts" setup>
+import type { Metadata } from '@tailor-cms/interfaces/schema';
+import { ref } from 'vue';
+import { RichTextEditor } from '@tailor-cms/core-components-next';
+
+interface Meta extends Metadata {
+  value?: string;
+}
+
+const props = defineProps<{ meta: Meta }>();
+
+const input = ref(props.meta.value ?? '');
+</script>
