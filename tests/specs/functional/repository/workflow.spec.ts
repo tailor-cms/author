d2ab59d773d658b3dac3e4e123137f387ba9664e
rkusan00 <renato.kusanovic@gmail.com>
2024-07-18 11:25:13 +0200

    Update test

--- tests/specs/functional/repository/workflow.spec.ts
+++ tests/specs/functional/repository/workflow.spec.ts
@@ -1,5 +1,7 @@
 import { expect, test } from '@playwright/test';
 import userSeed from 'tailor-seed/user.json';
+import { formatDate as format } from 'date-fns/format';
+import sample from 'lodash/sample';
 
 import {
   outlineLevel,
@@ -13,6 +15,12 @@ import { Workflow } from '../../../pom/workflow/Workflow';
 import { WorkflowSidebar } from '../../../pom/workflow/Sidebar';
 
 const getWorkflowRoute = (id) => `/repository/${id}/root/workflow`;
+const statuses = ['Todo', 'In progress', 'Review', 'Done'];
+const priorities = ['Trivial', 'Low', 'Medium', 'High', 'Critical'];
+const dateFormat = {
+  input: 'yyyy-MM-dd',
+  table: 'MMM d, yyyy',
+};
 
 test.beforeEach(async () => {
   await SeedClient.resetDatabase();
@@ -46,9 +54,9 @@ test('should be able to open sidebar for the workflow item', async ({
 
 test('should be able to update workflow item', async ({ page }) => {
   const name = outlineSeed.group.title;
-  const status = 'Review';
-  const priority = 'High';
-  const dueDate = '12/31/2023';
+  const status = sample(statuses);
+  const priority = sample(priorities);
+  const dueDate = new Date();
   const assignee = userSeed[0].email;
   await toSeededRepositoryWorkflow(page);
   const workflow = new Workflow(page);
@@ -58,14 +66,14 @@ test('should be able to update workflow item', async ({ page }) => {
   await sidebar.selectStatus(status);
   await sidebar.selectAssignee(assignee);
   await sidebar.selectPriority(priority);
-  await sidebar.selectDueDate(dueDate);
+  await sidebar.selectDueDate(format(dueDate, dateFormat.input));
   await page.reload();
   await page.waitForLoadState('networkidle');
   const row = await workflow.getItemByName(name);
   await expect(row.getByText(status)).toBeVisible();
   await expect(row.getByText(priority)).toBeVisible();
-  await expect(row.getByText('Dec 31, 2023')).toBeVisible();
   await expect(row.getByText(assignee)).toBeVisible();
+  await expect(row.getByText(format(dueDate, dateFormat.table))).toBeVisible();
 });
 
 test.afterAll(async () => {
