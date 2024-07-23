078ed22c504b44314e3c6dd1d9d1fda3a9cdc0e0
underscope <underscope@gmail.com>
2024-07-23 12:29:19 +0200

    🐐

--- apps/backend/activity/activity.controller.js
+++ apps/backend/activity/activity.controller.js
@@ -5,7 +5,7 @@ import get from 'lodash/get.js';
 import oauth2 from '../shared/oAuth2Provider.js';
 import pick from 'lodash/pick.js';
 import { previewUrl } from '../config/server/index.js';
-import { consumer: oAuthConfig } from '../config/server/index.js';
+import { consumer as oAuthConfig } from '../config/server/index.js';
 import publishingService from '../shared/publishing/publishing.service.js';
 import { schema } from 'tailor-config-shared';
 
