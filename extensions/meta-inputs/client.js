c224546cd73a513cd3874ba17af48a4dbdeab83d
rkusan00 <renato.kusanovic@gmail.com>
2024-08-16 11:40:06 +0200

    Install meta

--- extensions/meta-inputs/client.js
+++ extensions/meta-inputs/client.js
@@ -2,6 +2,7 @@ import pkg0 from '@tailor-cms/tme-textarea';
 import pkg1 from '@tailor-cms/tme-file';
 import pkg2 from '@tailor-cms/tme-color';
 import pkg3 from '@tailor-cms/tme-html';
+import pkg4 from '@tailor-cms/tme-radio-group';
 
 // prettier-ignore
 export const elements = [
@@ -9,4 +10,5 @@ export const elements = [
   pkg1,
   pkg2,
   pkg3,
+  pkg4,
 ];
