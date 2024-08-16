1ebd0495d419033e629a2f17f0587f24ca11050d
rkusan00 <renato.kusanovic@gmail.com>
2024-08-16 11:38:29 +0200

    Implement meta radio button

---
+++ packages/core-extensions/meta-inputs/meta-radio-group/src/EditMeta.vue
@@ -0,0 +1,18 @@
+<template>
+  <VRadioGroup
+    :label="meta.label"
+    :model-value="meta.value"
+    :name="meta.key"
+    class="my-2"
+    @change="$emit('update', meta.key, $event)"
+  >
+    <VRadio v-for="it in meta.items" :key="it.value" v-bind="it" />
+  </VRadioGroup>
+</template>
+
+<script lang="ts" setup>
+withDefaults(defineProps<{ meta?: any }>(), {
+  meta: () => ({ value: null }),
+});
+defineEmits(['update']);
+</script>
