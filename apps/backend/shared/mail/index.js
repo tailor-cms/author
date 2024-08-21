6a929c5ad090200d576a52fbee3381c49f838446
rkusan00 <renato.kusanovic@gmail.com>
2024-08-21 11:31:10 +0200

    Update path

--- apps/backend/shared/mail/index.js
+++ apps/backend/shared/mail/index.js
@@ -34,10 +34,10 @@ const activityStatusUrl = (repositoryId, activityId) =>
   urlJoin(
     origin,
     '/repository',
-    `${repositoryId}/progress?activityId=${activityId}`,
+    `${repositoryId}/root/workflow?activityId=${activityId}`,
   );
 const activityUrl = ({ repositoryId, activityId }) =>
-  urlJoin(origin, '/repository', `${repositoryId}?activityId=${activityId}`);
+  urlJoin(origin, '/repository', `${repositoryId}/root/structure?activityId=${activityId}`);
 const elementUrl = ({ repositoryId, activityId, elementUid }) => {
   const query = `${activityId}?elementId=${elementUid}`;
   return urlJoin(origin, '/repository', `${repositoryId}/editor`, query);
