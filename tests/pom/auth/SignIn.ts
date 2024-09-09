c8e414ca647d8ee30efeda71e7134d76903db5d9
rkusan00 <renato.kusanovic@gmail.com>
2024-09-09 17:09:55 +0200

    Add oidc e2e tests

--- tests/pom/auth/SignIn.ts
+++ tests/pom/auth/SignIn.ts
@@ -1,21 +1,27 @@
 import { Locator, Page } from '@playwright/test';
 
+import { OIDCSignIn } from './OIDCSignIn';
+
 export class SignIn {
   static route = '/auth';
   readonly page: Page;
+  readonly oidcBtn: Locator;
   readonly emailInput: Locator;
   readonly passwordInput: Locator;
   readonly forgotPasswordLink: Locator;
   readonly submitBtn: Locator;
+  readonly OIDCSignIn: OIDCSignIn;
 
   constructor(page: Page) {
     this.page = page;
+    this.oidcBtn = page.getByRole('button', { name: 'Sign in with OIDC' });
     this.emailInput = page.getByLabel('Email');
     this.passwordInput = page.getByLabel('Password');
     this.forgotPasswordLink = page.getByRole('link', {
       name: 'Forgot password?',
     });
-    this.submitBtn = page.getByRole('button', { name: 'Sign in' });
+    this.submitBtn = page.getByRole('button', { name: 'Sign in', exact: true });
+    this.OIDCSignIn = new OIDCSignIn(page);
   }
 
   visit() {
@@ -34,5 +40,10 @@ export class SignIn {
     await this.fillEmail(email);
     await this.fillPassword(password);
     await this.submitBtn.click();
+  }
+
+  async oidcSignIn(email?: string, password?: string) {
+    await this.oidcBtn.click();
+    await this.OIDCSignIn.signIn(email, password);
   }
 }
