99b8dd9e9aeeaa972b7b6c3c1cb7d7e0cf651a18
rkusan00 <renato.kusanovic@gmail.com>
2024-09-13 14:05:30 +0200

    Migrate combobox

---
+++ packages/core-extensions/meta-inputs/meta-combobox/src/EditMeta.vue
@@ -0,0 +1,36 @@
+<template>
+  <VCombobox
+    v-model="input"
+    :chips="meta.multiple"
+    :items="meta.options"
+    :label="meta.label"
+    :multiple="meta.multiple"
+    :name="meta.key"
+    :placeholder="meta.placeholder"
+    item-text="label"
+    item-value="value"
+    variant="outlined"
+    deletable-chips
+    @change="$emit('update', meta.key, input)"
+  />
+</template>
+
+<script lang="ts" setup>
+import type { Metadata } from '@tailor-cms/interfaces/schema';
+import { ref } from 'vue';
+
+interface Meta extends Metadata {
+  value?: string;
+}
+const props = defineProps<{ meta: Meta; dark: boolean }>();
+defineEmits(['update']);
+
+const input = ref(props.meta.value);
+
+// const value = computed(() => {
+//   const { meta: { value, options } } = this;
+//   const hasPrimitiveOptions = !isObject(options[0]);
+//   if (hasPrimitiveOptions) return value;
+//   return value.map(val => options.find(it => it.value === val));
+// }
+</script>
