45ae6702e469c6f9b4ee734e24f56f9541d0e049
rkusan00 <renato.kusanovic@gmail.com>
2024-09-13 11:32:02 +0200

    Implement meta checkbox

---
+++ packages/core-extensions/meta-inputs/meta-checkbox/src/EditMeta.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="control">
+    <div v-if="meta.label" class="control-label">{{ meta.label }}</div>
+    <VCheckbox
+      :input-value="meta.value"
+      :label="meta.description"
+      :name="meta.key"
+      class="mt-1"
+      color="primary darken-2"
+      @change="$emit('update', meta.key, $event)"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  meta?: any;
+}
+
+withDefaults(defineProps<Props>(), {
+  meta: () => ({ value: null }),
+});
+defineEmits(['update']);
+</script>
+
+<style lang="scss" scoped>
+.control {
+  padding: 3px 8px;
+
+  ::v-deep label.v-label {
+    margin-bottom: 0;
+  }
+
+  &-label {
+    font-size: 0.875rem;
+    font-weight: normal;
+    text-align: left;
+    color: #0000008a;
+  }
+}
+</style>
