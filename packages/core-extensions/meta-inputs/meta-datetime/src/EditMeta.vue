a7ffda681dd1c4338639d5b597ab582de4565290
rkusan00 <renato.kusanovic@gmail.com>
2024-09-19 09:08:39 +0200

    Add package

---
+++ packages/core-extensions/meta-inputs/meta-datetime/src/EditMeta.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ input }}</div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import type { Metadata } from '@tailor-cms/interfaces/schema';
+
+interface Meta extends Metadata {
+  value?: string;
+}
+const props = defineProps<{ meta: Meta; dark: boolean }>();
+defineEmits(['update']);
+
+const input = ref(props.meta.value);
+</script>
