f31e00ed1ce312ae66ef5e80ec342c030d8989bf
rkusan00 <renato.kusanovic@gmail.com>
2024-09-30 15:25:55 +0200

    Install text response and true-false

--- extensions/content-elements/client.js
+++ extensions/content-elements/client.js
@@ -3,6 +3,8 @@ import pkg1 from '@tailor-cms/ce-image-edit';
 import pkg2 from '@tailor-cms/ce-multiple-choice-edit';
 import pkg3 from '@tailor-cms/ce-single-choice-edit';
 import pkg4 from '@tailor-cms/ce-matching-question-edit';
+import pkg5 from '@tailor-cms/ce-text-response-edit';
+import pkg6 from '@tailor-cms/ce-true-false-edit';
 
 // prettier-ignore
 export const elements = [
@@ -11,4 +13,6 @@ export const elements = [
   pkg2,
   pkg3,
   pkg4,
+  pkg5,
+  pkg6,
 ];
