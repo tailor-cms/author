514a74bef359ead4ed3e361c5c86e5ec5af37525
rkusan00 <renato.kusanovic@gmail.com>
2024-10-01 08:48:48 +0200

    Add new packages to server

--- extensions/content-elements/server.js
+++ extensions/content-elements/server.js
@@ -3,6 +3,8 @@ import pkg1 from '@tailor-cms/ce-image-server';
 import pkg2 from '@tailor-cms/ce-multiple-choice-server';
 import pkg3 from '@tailor-cms/ce-single-choice-server';
 import pkg4 from '@tailor-cms/ce-matching-question-server';
+import pkg5 from '@tailor-cms/ce-text-response-server';
+import pkg6 from '@tailor-cms/ce-true-false-server';
 
 // prettier-ignore
 export const elements = [
@@ -11,4 +13,6 @@ export const elements = [
   pkg2,
   pkg3,
   pkg4,
+  pkg5,
+  pkg6,
 ];
