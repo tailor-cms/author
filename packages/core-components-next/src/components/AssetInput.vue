e66dc46549992ff261ff7cdf8edc535204221cd3
rkusan00 <renato.kusanovic@gmail.com>
2024-10-19 01:19:10 +0200

    Implement AssetInput & UploadBtn

---
+++ packages/core-components-next/src/components/AssetInput.vue
@@ -0,0 +1,125 @@
+<template>
+  <VToolbarItems>
+    <VBtn
+      v-if="url && !isEditing"
+      :href="publicUrl || url"
+      color="info"
+      icon="mdi-open-in-new"
+      target="_blank"
+      variant="text"
+    />
+    <UploadBtn
+      v-if="allowFileUpload"
+      :extensions="extensions"
+      :file-name="fileName"
+      :is-editing="isEditing"
+      :label="props.uploadLabel"
+      @delete="file = null"
+      @update:uploading="uploading = $event"
+      @upload="uploadFile"
+    />
+    <div v-if="!uploading && (urlInput || !hasAsset)">
+      <VTextField
+        v-model="urlInput"
+        :disabled="!isEditing"
+        :error-messages="errors.url"
+        :placeholder="allowFileUpload ? 'or paste a URL...' : 'Paste a URL...'"
+        hide-details="auto"
+        min-width="350"
+        variant="outlined"
+        clearable
+      />
+    </div>
+    <VBtn v-if="!isEditing" @click="isEditing = true">Edit</VBtn>
+    <template v-else>
+      <VBtn v-if="hasChanges" :disabled="uploading" @click="save">Save</VBtn>
+      <VBtn v-if="hasChanges || url" :disabled="uploading" @click="cancel">
+        Cancel
+      </VBtn>
+    </template>
+  </VToolbarItems>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+import { object, string } from 'yup';
+import last from 'lodash/last';
+import pick from 'lodash/pick';
+import { useForm } from 'vee-validate';
+
+import UploadBtn from './UploadBtn.vue';
+
+const isUploaded = (url: string | null) => {
+  try {
+    return url && new URL(url).protocol === 'storage:';
+  } catch (e) {
+    return false;
+  }
+};
+
+interface Props {
+  extensions: string[];
+  url?: string | null;
+  publicUrl?: string | null;
+  allowFileUpload?: boolean;
+  uploadLabel?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  url: null,
+  publicUrl: null,
+  allowFileUpload: true,
+  uploadLabel: 'Select file',
+});
+const emit = defineEmits(['input']);
+
+const uploading = ref(false);
+const isEditing = ref(!props.url);
+const isLinked = ref(!isUploaded(props.url));
+const file = ref(isLinked.value ? null : pick(props, ['url', 'publicUrl']));
+
+const { defineField, errors, validate, resetForm } = useForm({
+  validationSchema: object({
+    url: string()
+      .nullable()
+      .url({ protocols: ['http', 'https'], require_protocol: true }),
+  }),
+  initialValues: { url: isLinked.value ? props.url : null },
+});
+
+const [urlInput] = defineField('url');
+
+const hasAsset = computed(() => file.value || urlInput.value);
+const hasChanges = computed(
+  () => props.url !== (urlInput.value || file.value?.url || null),
+);
+
+const fileName = computed(() => {
+  if (!file.value || !file.value.url) return;
+  return last(file.value.url.split('___'));
+});
+
+const uploadFile = (value: any) => {
+  file.value = value;
+  urlInput.value = null;
+  resetForm();
+};
+
+const save = async () => {
+  const { valid } = await validate();
+  if (!valid) return;
+  isEditing.value = false;
+  const payload = file.value || {
+    url: urlInput.value,
+    publicUrl: urlInput.value,
+  };
+  emit('input', payload);
+};
+
+const cancel = () => {
+  urlInput.value = isLinked.value ? props.url : null;
+  file.value = isLinked.value ? null : pick(props, ['url', 'publicUrl']);
+  isEditing.value = !props.url;
+  resetForm();
+};
+</script>
