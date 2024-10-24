3761488c78730167475b59be47fa2f510e5babea
rkusan00 <renato.kusanovic@gmail.com>
2024-10-24 08:02:34 +0200

    Migrate Question Feedback component

--- packages/core-components-next/src/index.ts
+++ packages/core-components-next/src/index.ts
@@ -14,5 +14,6 @@ export { default as RichTextEditor } from './components/RichTextEditor/index.vue
 export { default as AssetInput } from './components/AssetInput.vue';
 export { default as ElementPlaceholder } from './components/ElementPlaceholder.vue';
 export { default as PreviewOverlay } from './components/PreviewOverlay.vue';
+export { default as QuestionFeedback } from './components/QuestionFeedback.vue';
 export { useLoader } from './composables/useLoader';
 export { useConfirmationDialog } from './composables/useConfirmationDialog';
