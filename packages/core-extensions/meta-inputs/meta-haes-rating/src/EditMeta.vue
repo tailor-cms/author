bc1884df9a8162cd4a77390afad8e94c5ea54610
rkusan00 <renato.kusanovic@gmail.com>
2024-10-30 22:35:18 +0100

    Cleanup

--- packages/core-extensions/meta-inputs/meta-haes-rating/src/EditMeta.vue
+++ packages/core-extensions/meta-inputs/meta-haes-rating/src/EditMeta.vue
@@ -1,13 +1,58 @@
 <template>
-  <VCard>
-    <VCardText>
-      <template v-for="(param, i) in params" :key="param">
-        <VLabel>{{ param }}</VLabel>
-        <VSlider v-model="input[i]" max="4" min="0" step="0.1" />
-      </template>
-      <RadarChart :data="chartData" />
-    </VCardText>
-  </VCard>
+  <div class="ma-1">
+    <div class="d-flex align-center justify-space-between">
+      <div class="text-caption mb-2">{{ meta.label }}</div>
+      <VBtnToggle
+        v-model="mode"
+        :base-color="dark ? 'primary-lighten-4' : 'primary-darken-4'"
+        density="compact"
+        rounded="lg"
+        variant="text"
+        border
+      >
+        <VBtn
+          :value="RatingMode.Slider"
+          class="px-4"
+          icon="mdi-tune"
+          size="small"
+        />
+        <VBtn
+          :value="RatingMode.Chart"
+          class="px-4"
+          icon="mdi-radar"
+          size="small"
+        />
+      </VBtnToggle>
+    </div>
+    <div v-if="mode === RatingMode.Slider">
+      <div
+        v-for="(label, i) in labels"
+        :key="label"
+        class="d-flex align-center"
+      >
+        <div class="label text-caption">{{ label }}</div>
+        <VSlider
+          v-model="input[i]"
+          :color="dark ? 'white' : 'primary'"
+          max="4"
+          min="0"
+          step="0.1"
+          hide-details
+          thumb-label
+          @end="onChange"
+        />
+        <div class="text-subtitle-2 ml-2 slider-value">{{ input[i] }}</div>
+      </div>
+    </div>
+    <VSheet
+      v-else
+      class="d-flex justify-center"
+      color="transparent"
+      height="350"
+    >
+      <RadarChart :dark="dark" :data="chartData" />
+    </VSheet>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -15,11 +60,17 @@ import { computed, ref } from 'vue';
 
 import RadarChart from './RadarChart.vue';
 
-interface Props {
-  meta?: any;
+enum RatingMode {
+  Slider = 'SLIDER',
+  Chart = 'CHART',
 }
 
-const params = [
+interface Props {
+  meta?: any;
+  dark?: boolean;
+}
+
+const labels = [
   'Learner Centered Content',
   'Active Learning',
   'Unbounded Inclusion',
@@ -27,26 +78,43 @@ const params = [
   'Real-World Outcomes',
 ];
 
+const mode = ref(RatingMode.Slider);
+
 const props = withDefaults(defineProps<Props>(), {
   meta: () => ({ value: null }),
+  dark: false,
 });
-// const emit = defineEmits(['update']);
+const emit = defineEmits(['update']);
 
-const input = ref(props.meta.value || [2.5, 2.5, 2.5, 2.5, 2.5]);
+const input = ref([...(props.meta.value || new Array(5).fill(2.5))]);
 
-const chartData = computed(() => {
-  return {
-    labels: params,
-    datasets: [
-      {
-        data: [...input.value],
-      },
-    ],
-  };
-});
+const chartData = computed(() => ({
+  labels: labels.map((it) => it.split(' ')),
+  datasets: [
+    {
+      data: input.value,
+      backgroundColor: 'rgba(233, 30, 99, 0.2)',
+      borderColor: 'rgba(233, 30, 99, 0.7)',
+      borderWidth: 2,
+    },
+  ],
+}));
 
-// const onChange = () => {
-//   if (input.value === props.meta.value) return;
-//   emit('update', props.meta.key, input.value);
-// };
+const onChange = () => {
+  if (input.value === props.meta.value) return;
+  emit('update', props.meta.key, input.value);
+};
 </script>
+
+<style lang="scss" scoped>
+.label {
+  width: 10rem;
+  opacity: 0.7;
+}
+
+.slider-value {
+  width: 1.25rem;
+  opacity: 0.7;
+  text-align: right;
+}
+</style>
--- packages/core-extensions/meta-inputs/meta-haes-rating/src/RadarChart.vue
+++ packages/core-extensions/meta-inputs/meta-haes-rating/src/RadarChart.vue
@@ -1,9 +1,10 @@
 <template>
-  <Radar id="my-chart-id" :data="data" :options="chartOptions" />
+  <Radar :data="data" :options="chartOptions" />
 </template>
 
 <script lang="ts" setup>
 import {
+  ChartDataset,
   Chart as ChartJS,
   Filler,
   LineElement,
@@ -11,31 +12,44 @@ import {
   RadialLinearScale,
   Tooltip,
 } from 'chart.js';
+import { computed } from 'vue';
 import { Radar } from 'vue-chartjs';
-import { reactive } from 'vue';
 
 ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip);
 
-defineProps<{
+const props = defineProps<{
+  dark: boolean;
   data: {
-    labels: string[];
-    datasets: { data: number[] }[];
+    labels: string[] | string[][];
+    datasets: ChartDataset<'radar', (number | null)[]>[];
   };
 }>();
 
-const chartOptions = reactive({
+const chartOptions = computed(() => ({
   responsive: true,
   scales: {
     r: {
+      pointLabels: {
+        color: props.dark ? 'rgb(236, 239, 241)' : 'black',
+        font: { size: 12 },
+      },
+      grid: {
+        color: props.dark ? 'rgba(236, 239, 241, 0.38)' : 'rgba(0, 0, 0, 0.25)',
+        circular: true,
+      },
       angleLines: {
-        display: false,
+        color: props.dark ? 'rgba(236, 239, 241, 0.38)' : 'rgba(0, 0, 0, 0.25)',
       },
       ticks: {
         count: 5,
+        backdropColor: 'rgba(0, 0, 0, 0)',
+        color: props.dark ? 'white' : 'black',
+        font: { size: 16 },
+        position: 'inside',
       },
       min: 0,
       max: 4,
     },
   },
-});
+}));
 </script>
