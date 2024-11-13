91890bcf046dd0576792529f946f294e7931d71e
underscope <underscope@gmail.com>
2024-11-13 06:58:53 +0100

    🧹

--- apps/frontend/plugins/statsig.ts
+++ apps/frontend/plugins/statsig.ts
@@ -6,8 +6,6 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.provide('statsigInit', async (sdkSecret: string, userID: string) => {
     if (!sdkSecret || !userID) return null;
     const client = new StatsigClient(sdkSecret, { userID });
-    // Remove these lines if you dont want to use session replay
-    // or web analytics
     runStatsigSessionReplay(client);
     runStatsigAutoCapture(client);
     await client.initializeAsync();
