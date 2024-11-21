c1d82e45badf1885703c3f3748480f89670ca558
underscope <underscope@gmail.com>
2024-11-21 11:38:19 +0100

    💄 Lint

--- apps/backend/activity/status.model.js
+++ apps/backend/activity/status.model.js
@@ -1,7 +1,7 @@
 import { Activity as Events } from 'sse-event-types';
 import hooks from './status.hooks.js';
 import { Model } from 'sequelize';
-import { workflow } from '@tailor-cms/config'
+import { workflow } from '@tailor-cms/config';
 
 const { priorities } = workflow;
 const priorityIds = priorities.map((it) => it.id);
