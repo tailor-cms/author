a11ca4676926a0479d39511570a1e3efc027a807
underscope <underscope@gmail.com>
2024-12-04 14:02:35 +0100

    Init container meta resolver

---
+++ packages/core-extensions/content-containers/structured-content/src/edit/MetaInput.vue
@@ -0,0 +1,59 @@
+<template>
+  <component
+    :is="componentName"
+    :class="{ required: get(meta, 'validate.required') }"
+    :error-messages="errorMessage"
+    :dark="false"
+    :meta="meta"
+    :is-new="isNew"
+    @update="updateMeta"
+  />
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+import get from 'lodash/get';
+import { getMetaName } from '@tailor-cms/utils';
+import { useField } from 'vee-validate';
+
+import type { Metadata } from '@tailor-cms/interfaces/schema';
+
+interface Props {
+  meta: Metadata;
+  name?: string | null;
+  isNew?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  name: null,
+  isNew: false,
+});
+
+const emit = defineEmits(['update']);
+
+const type = computed(() => props.meta.type.toUpperCase());
+const componentName = computed(() => getMetaName(type.value));
+
+const { errorMessage, handleChange, validate } = useField(
+  () => props.meta.key,
+  props.meta.validate,
+  {
+    label: props.meta.key,
+    initialValue: props.meta.value,
+  },
+);
+
+const updateMeta = async (key: string, value: any) => {
+  handleChange(value, false);
+  const { valid } = await validate();
+  if (valid) emit('update', key, value);
+};
+</script>
+
+<style lang="scss" scoped>
+:deep(.title) {
+  color: #808080;
+  font-size: 0.875rem !important;
+  font-weight: normal;
+}
+</style>
