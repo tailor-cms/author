90655c22d4c325fab81ed98beba7930295c65556
rkusan00 <renato.kusanovic@gmail.com>
2025-01-17 12:51:26 +0100

    Implement assessment item

---
+++ packages/core-components/src/components/AssessmentItem.vue
@@ -0,0 +1,192 @@
+<template>
+  <VHover v-slot="{ isHovering: isCardHovered, props: hoverProps }">
+    <div
+      v-bind="hoverProps"
+      :class="[props.element.changeSincePublish, {
+        hover: isCardHovered,
+        expanded: props.expanded,
+        diff: editorState.isPublishDiff,
+      }]"
+      class="assessment-item">
+      <span v-if="draggable" class="drag-handle">
+        <VIcon>mdi-drag-vertical</VIcon>
+      </span>
+      <component
+        :is="componentName(element.type)"
+        v-if="props.expanded"
+        v-bind="{
+          element,
+          embedTypes: embedElementConfig,
+          embedElementConfig,
+          isDisabled,
+        }"
+        @delete="emit('delete')"
+        @save="save"
+      />
+      <VCard
+        v-else
+        class="d-flex justify-space-between align-center px-4 py-2"
+        color="primary-darken-2"
+        variant="flat"
+        @click="$emit('selected')">
+        <div>
+          <VIcon
+            :icon="elementConfig?.ui.icon"
+            color="secondary-lighten-2"
+            start
+          />
+          <span class="text-subtitle-1">{{ elementConfig?.name }}</span>
+        </div>
+        <span class="question">{{ question }}</span>
+        <PublishDiffChip
+          v-if="editorState.isPublishDiff && props.element.changeSincePublish"
+          :change-type="props.element.changeSincePublish" />
+        <VBtn
+          v-else
+          :class="{ disabled: props.isDisabled }"
+          color="secondary-lighten-3"
+          class="delete"
+          variant="tonal"
+          size="x-small"
+          icon
+          @click.stop="$emit('delete')">
+          <VIcon icon="mdi-delete-outline" size="large" />
+        </VBtn>
+      </VCard>
+    </div>
+  </VHover>
+</template>
+
+<script lang="ts" setup>
+import { computed, inject, ref } from 'vue';
+import type {
+  ContentElementCategory,
+  ElementRegistry,
+} from '@tailor-cms/interfaces/schema';
+import type { ContentElement } from '@tailor-cms/interfaces/content-element';
+import { getComponentName } from '@tailor-cms/utils';
+import cloneDeep from 'lodash/cloneDeep';
+import filter from 'lodash/filter';
+import map from 'lodash/map';
+import truncate from 'lodash/truncate';
+
+import PublishDiffChip from './PublishDiffChip.vue';
+
+interface Props {
+  embedElementConfig?: ContentElementCategory[] | null;
+  element: ContentElement;
+  expanded?: boolean;
+  draggable?: boolean;
+  isDisabled?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  expanded: false,
+  draggable: false,
+  isDisabled: false,
+});
+
+const emit = defineEmits(['add', 'save', 'delete', 'selected']);
+
+const editorState = inject<any>('$editorState');
+const ceRegistry = inject<ElementRegistry>('$ceRegistry');
+
+const TEXT_CONTAINERS = ['CE_HTML_DEFAULT'];
+const blankRegex = /(@blank)/g;
+const htmlRegex = /(<\/?[^>]+(>|$))|&nbsp;/g;
+
+const elementConfig = computed(() => ceRegistry?.get(props.element.type));
+
+const getTextAssets = (item: any) =>
+  filter(item, (it: any) => TEXT_CONTAINERS.includes(it.type));
+
+const componentName = (type: string) => getComponentName(type);
+
+const save = (data: any) => {
+  emit('save', { ...cloneDeep(props.element), data });
+};
+
+const question = computed(() => {
+  const { question, embeds } = props.element.data;
+  const textAssets = getTextAssets(question.map((id) => embeds[id]));
+  const questionText = map(textAssets, 'data.content').join(' ');
+  return truncate(
+    questionText.replace(htmlRegex, '').replace(blankRegex, () => '____'),
+    { length: 50 });
+});
+</script>
+
+<style lang="scss" scoped>
+@use '../mixins';
+
+.assessment-item {
+  margin-bottom: 0.625rem;
+  padding: 0;
+
+  .v-chip {
+    min-width: 1.875rem;
+  }
+
+  .drag-handle {
+    position: absolute;
+    top: 0;
+    left: -3px;
+    opacity: 0;
+    cursor: move;
+    font-size: 28px;
+    color: #888;
+  }
+
+  &.hover .drag-handle {
+    opacity: 1;
+    transition: opacity 0.6s ease-in-out;
+  }
+
+  .minimized {
+    padding: 0.375rem 1.375rem;
+    cursor: pointer;
+
+    .question {
+      display: inline-block;
+      max-width: 80%;
+      min-height: 1.875rem;
+      font-size: 1rem;
+      line-height: 2.125rem;
+      font-weight: 400;
+      color: #444;
+    }
+
+    .v-chip {
+      margin-top: 0.125rem;
+    }
+  }
+
+  .delete {
+    opacity: 0;
+  }
+
+  &.hover:not(.sortable-chosen) .delete:not(.disabled) {
+    opacity: 1;
+  }
+}
+
+.question-container {
+  margin: 0 !important;
+}
+
+.diff {
+  border: none;
+
+  &.expanded {
+    border-radius: 4px;
+  }
+
+  &.new {
+    @include mixins.highlight(var(--v-success-lighten2));
+  }
+
+  &.changed, &.removed {
+    @include mixins.highlight(var(--v-secondary-lighten4));
+  }
+}
+</style>
