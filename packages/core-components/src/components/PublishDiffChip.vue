37a89eef485af324f5e7aae34555d31370eea81a
rkusan00 <renato.kusanovic@gmail.com>
2025-01-20 11:34:13 +0100

    Update publish chip color

--- packages/core-components/src/components/PublishDiffChip.vue
+++ packages/core-components/src/components/PublishDiffChip.vue
@@ -1,7 +1,7 @@
 <template>
   <VChip
     v-if="changeType"
-    :color="changeType === 'new' ? 'success' : 'secondary'"
+    :color="chipColor"
     :variant="variant"
     class="readonly font-weight-medium text-capitalize"
     size="small"
@@ -12,14 +12,22 @@
 </template>
 
 <script lang="ts" setup>
+import { PublishDiffChangeTypes } from '@tailor-cms/utils';
+import { computed } from 'vue';
+
 // TODO: Update the type once utils is migrated to ts
 interface Props {
-  changeType?: string | null;
+  changeType?: PublishDiffChangeTypes | null;
   variant?: string;
 }
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
   changeType: null,
   variant: 'tonal',
+});
+
+const chipColor = computed(() => {
+  const isNewItem = props.changeType === PublishDiffChangeTypes.New;
+  return isNewItem ? 'green-accent-4' : 'pink-accent-1';
 });
 </script>
