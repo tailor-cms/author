67a77e52e5261344726d8d0446d243dea0514e95
rkusan00 <renato.kusanovic@gmail.com>
2025-02-03 18:28:03 +0100

    Update lx question container

--- packages/lx-components/src/components/QuestionContainer/QuestionPrompt.vue
+++ packages/lx-components/src/components/QuestionContainer/QuestionPrompt.vue
@@ -4,11 +4,12 @@
 
 <script setup lang="ts">
 import { computed } from 'vue';
+import sortBy from 'lodash/sortBy';
 
 const props = defineProps<{
   question: string[];
   embeds: Record<string, any>;
 }>();
 
-const elements = computed(() => props.question.map((id) => props.embeds[id]));
+const elements = computed(() => sortBy(props.embeds, 'position'));
 </script>
