ccc2c32086e01d706e528bbea3b65c5b53df3465
rkusan00 <renato.kusanovic@gmail.com>
2025-02-04 15:50:58 +0100

    Revert structure

--- packages/lx-components/src/components/QuestionContainer/QuestionPrompt.vue
+++ packages/lx-components/src/components/QuestionContainer/QuestionPrompt.vue
@@ -4,11 +4,11 @@
 
 <script setup lang="ts">
 import { computed } from 'vue';
-import sortBy from 'lodash/sortBy';
 
 const props = defineProps<{
+  question: string[];
   embeds: Record<string, any>;
 }>();
 
-const elements = computed(() => sortBy(props.embeds, 'position'));
+const elements = computed(() => props.question.map((id) => props.embeds[id]));
 </script>
