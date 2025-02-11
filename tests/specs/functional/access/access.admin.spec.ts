732e1353b162898c935669bebab853f86dac3d78
underscope <underscope@gmail.com>
2025-02-11 13:26:18 +0100

    Test create repo & admin access

--- tests/specs/functional/access/access.admin.spec.ts
+++ tests/specs/functional/access/access.admin.spec.ts
@@ -1,5 +1,7 @@
 import { expect, test } from '@playwright/test';
 
+import { AddRepositoryDialog } from '../../../pom/catalog/AddRepository.ts';
+import { AppBar } from '../../../pom/common/AppBar.ts';
 import {
   UserManagement,
 } from '../../../pom/admin/UserManagement.ts';
@@ -22,27 +24,41 @@ test('As a Admin I should be able to see 4 menu entries', async ({ page }) => {
   await expect(sidebarLocator).toContainText('Installed Elements');
 });
 
-test('I should be able to access user management as an admin', async ({ page }) => {
+test('as an Admin, I should be able to create Repository', async ({
+  page,
+}) => {
+  await page.goto('/');
+  const dialog = new AddRepositoryDialog(page);
+  await expect(dialog.openDialogBtn).toBeVisible();
+});
+
+test('as an Admin, I should see the admin menu entry', async ({ page }) => {
+  await page.goto('/');
+  const appBar = new AppBar(page);
+  await expect(appBar.adminLink).toBeVisible();
+});
+
+test('as an Admin, I should be able to access user management', async ({ page }) => {
   await page.goto(UserManagement.route);
   await expect(page).toHaveURL(UserManagement.route);
   const userManagement = new UserManagement(page);
   await expect(userManagement.el).toBeVisible();
 });
 
-test('I should be able to access group management as an admin', async ({ page }) => {
+test('as an Admin, I should be able to access group management', async ({ page }) => {
   await page.goto(GroupManagement.route);
   await expect(page).toHaveURL(GroupManagement.route);
   const groupManagement = new GroupManagement(page);
   await expect(groupManagement.el).toBeVisible();
 });
 
-test('I should be able to access Structure Types', async ({ page }) => {
+test('as an Admin, I should be able to access Structure Types', async ({ page }) => {
   await page.goto('/admin/structure-types');
   await page.waitForLoadState('networkidle');
   await expect(page).toHaveURL('/admin/structure-types');
 });
 
-test('I should be able to access Installed Elements', async ({ page }) => {
+test('as an Admin, I should be able to access Installed Elements', async ({ page }) => {
   await page.goto('/admin/installed-elements');
   await page.waitForLoadState('networkidle');
   await expect(page).toHaveURL('/admin/installed-elements');
--- tests/specs/functional/access/access.collaborator.spec.ts
+++ tests/specs/functional/access/access.collaborator.spec.ts
@@ -1,5 +1,7 @@
 import { expect, test } from '@playwright/test';
 
+import { AddRepositoryDialog } from '../../../pom/catalog/AddRepository.ts';
+import { AppBar } from '../../../pom/common/AppBar.ts';
 import { GroupManagement } from '../../../pom/admin/GroupManagement.ts';
 import { UserManagement } from '../../../pom/admin/UserManagement.ts';
 import SeedClient from '../../../api/SeedClient.ts';
@@ -8,7 +10,23 @@ test.beforeAll(async () => {
   await SeedClient.resetDatabase();
 });
 
-test('As a Collaborator I should not be able to access User Management', async ({
+test('as a Collaborator, I should not be able to create Repository', async ({
+  page,
+}) => {
+  await page.goto('/');
+  const dialog = new AddRepositoryDialog(page);
+  await expect(dialog.openDialogBtn).not.toBeVisible();
+});
+
+test('as a Collaborator, I should not see the Admin menu entry', async ({
+  page,
+}) => {
+  await page.goto('/');
+  const appBar = new AppBar(page);
+  await expect(appBar.adminLink).not.toBeVisible();
+});
+
+test('as a Collaborator, I should not be able to access User Management', async ({
   page,
 }) => {
   await page.goto(UserManagement.route);
@@ -16,7 +34,7 @@ test('As a Collaborator I should not be able to access User Management', async (
   await expect(page).toHaveURL('/');
 });
 
-test('As a Collaborator I should not be able to access Group management', async ({
+test('as a Collaborator, I should not be able to access Group management', async ({
   page,
 }) => {
   await page.goto(GroupManagement.route);
@@ -24,7 +42,7 @@ test('As a Collaborator I should not be able to access Group management', async 
   await expect(page).toHaveURL('/');
 });
 
-test('As a Collaborator I should not be able to access Structure Types', async ({
+test('as a Collaborator, I should not be able to access Structure Types', async ({
   page,
 }) => {
   await page.goto('/admin/structure-types');
@@ -32,7 +50,7 @@ test('As a Collaborator I should not be able to access Structure Types', async (
   await expect(page).toHaveURL('/');
 });
 
-test('As a Collaborator I should not be able to access Installed Elements', async ({
+test('as a Collaborator, I should not be able to access Installed Elements', async ({
   page,
 }) => {
   await page.goto('/admin/installed-elements');
