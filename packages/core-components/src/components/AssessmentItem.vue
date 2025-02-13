871e913cc6e7461cb1433ddc899093bc134afe1a
rkusan00 <renato.kusanovic@gmail.com>
2025-02-13 11:29:23 +0100

    Move delete button

--- packages/core-components/src/components/AssessmentItem.vue
+++ packages/core-components/src/components/AssessmentItem.vue
@@ -1,89 +1,90 @@
 <template>
-  <VHover v-slot="{ isHovering: isCardHovered, props: hoverProps }">
-    <div
-      v-bind="hoverProps"
-      class="d-flex align-start justify-strech">
-      <span v-if="!isDisabled && draggable" class="drag-handle">
-        <VIcon
-          icon="mdi-drag-vertical"
-          color="primary-lighten-2"
-          size="large"
-        />
-      </span>
-      <QuestionElement
-        v-if="expanded"
-        :icon="elementConfig?.ui.icon"
-        :type="elementConfig?.name"
-        v-bind="{
-          componentName,
-          element,
-          embedElementConfig,
-          isDisabled,
-        }"
-        :class="[element.changeSincePublish, { diff: showPublishDiff }]"
-        class="flex-grow-1"
-        @delete="emit('delete')"
-        @save="save"
-      >
-        <template #default>
-          <div class="d-flex px-6 py-4">
-            <slot name="header"></slot>
-          </div>
-        </template>
-      </QuestionElement>
-      <VCard
-        v-else
-        :class="[element.changeSincePublish, { diff: showPublishDiff }]"
-        class="d-flex justify-space-between align-center pa-2 w-100"
-        color="primary-darken-2"
-        variant="flat"
-        min-height="48"
-        @click="$emit('selected')">
-        <VRow class="w-100" dense>
-          <VCol cols="3" class="text-left align-content-center">
-            <div class="px-2 d-flex align-center">
-              <VIcon
-                :icon="elementConfig?.ui.icon"
-                color="secondary-lighten-2"
-                size="small"
-                start
-              />
-              <span class="text-subtitle-1">{{ elementConfig?.name }}</span>
-            </div>
-          </VCol>
-          <VCol
-            cols="6"
-            class="text-subtitle-2 align-content-center text-truncate"
-          >
-            {{ question }}
-          </VCol>
-          <VCol cols="3" class="text-right align-content-center">
-            <PublishDiffChip
-              v-if="editorState.isPublishDiff && element.changeSincePublish"
-              :change-type="publishDiffChangeType"
-            />
-            <VBtn
-              v-else-if="isCardHovered"
-              color="secondary-lighten-3"
-              class="delete"
-              variant="tonal"
-              size="x-small"
-              icon
-              @click.stop="$emit('delete')">
-              <VIcon icon="mdi-delete-outline" size="large" />
-            </VBtn>
-          </VCol>
-        </VRow>
-      </VCard>
-      <VBtn
-        icon="mdi-chevron-down"
-        variant="tonal"
-        class="ml-3 my-1"
-        density="comfortable"
+  <div class="d-flex align-start justify-strech">
+    <span v-if="!isDisabled && draggable" class="drag-handle">
+      <VIcon
+        icon="mdi-drag-vertical"
         color="primary-lighten-2"
-        @click="$emit('selected')" />
+        size="large"
+      />
+    </span>
+    <QuestionElement
+      v-if="expanded"
+      :icon="elementConfig?.ui.icon"
+      :type="elementConfig?.name"
+      v-bind="{
+        componentName,
+        element,
+        embedElementConfig,
+        isDisabled,
+      }"
+      :class="[element.changeSincePublish, { diff: showPublishDiff }]"
+      class="flex-grow-1"
+      @delete="emit('delete')"
+      @save="save"
+    >
+      <template #default>
+        <div class="d-flex px-6 py-4">
+          <slot name="header"></slot>
+        </div>
+      </template>
+    </QuestionElement>
+    <VCard
+      v-else
+      :class="[element.changeSincePublish, { diff: showPublishDiff }]"
+      class="d-flex justify-space-between align-center pa-2 w-100"
+      color="primary-darken-2"
+      variant="flat"
+      min-height="48"
+      @click="$emit('selected')">
+      <VRow class="w-100" dense>
+        <VCol cols="3" class="text-left align-content-center">
+          <div class="px-2 d-flex align-center">
+            <VIcon
+              :icon="elementConfig?.ui.icon"
+              color="secondary-lighten-2"
+              size="small"
+              start
+            />
+            <span class="text-subtitle-1">{{ elementConfig?.name }}</span>
+          </div>
+        </VCol>
+        <VCol
+          cols="6"
+          class="text-subtitle-2 align-content-center text-truncate"
+        >
+          {{ question }}
+        </VCol>
+        <VCol cols="3" class="text-right align-content-center">
+          <PublishDiffChip
+            v-if="editorState.isPublishDiff && element.changeSincePublish"
+            :change-type="publishDiffChangeType"
+          />
+        </VCol>
+      </VRow>
+    </VCard>
+    <div class="d-flex flex-column ml-3 my-1">
+      <VBtn
+        color="primary-lighten-2"
+        density="comfortable"
+        :icon="`mdi-chevron-${expanded ? 'up' : 'down'}`"
+        variant="tonal"
+        @click="$emit('selected')"
+      />
+      <VSlideYTransition>
+        <VBtn
+          v-if="expanded"
+          class="mt-2"
+          color="secondary-lighten-2"
+          density="comfortable"
+          variant="tonal"
+          icon
+          @click.stop="$emit('delete')"
+        >
+          <VIcon icon="mdi-delete-outline" size="small" />
+        </VBtn>
+      </VSlideYTransition>
     </div>
-  </VHover>
+  </div>
 </template>
 
 <script lang="ts" setup>
