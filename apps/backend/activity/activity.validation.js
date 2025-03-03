9913bc81e3cebf4ff83200f75413e88b0f79b88c
underscope <underscope@gmail.com>
2025-03-03 11:59:26 +0100

    Add request validation

---
+++ apps/backend/activity/activity.validation.js
@@ -0,0 +1,64 @@
+import { body, param, query } from 'express-validator';
+import defineRequestValidator from '#shared/request/validation.js';
+
+const entityPathParams = [
+  param('repositoryId').isInt(),
+  param('activityId').isInt(),
+];
+
+export const list = defineRequestValidator([
+  param('repositoryId').isInt(),
+  // Include items for which parent is deleted
+  query('detached').isBoolean().optional(),
+  // Include only outline activities (type declared in the schema structure)
+  query('outlineOnly').isBoolean().optional(),
+]);
+
+export const create = defineRequestValidator([
+  param('repositoryId').isInt(),
+  body('uid').isUUID().optional(),
+  body('parentId').isInt().optional(),
+  body('type').isString().trim().escape().notEmpty(),
+  body('position').isFloat().optional(),
+  body('data').isObject().optional(),
+  body('refs').isObject().optional(),
+]);
+
+export const get = defineRequestValidator(entityPathParams);
+
+export const update = defineRequestValidator([
+  ...entityPathParams,
+  body('position').isFloat().optional(),
+  body('data').isObject().optional(),
+  body('refs').isObject().optional(),
+]);
+
+export const remove = defineRequestValidator(entityPathParams);
+
+export const reorder = defineRequestValidator([
+  ...entityPathParams,
+  body('position').isFloat(),
+]);
+
+export const restore = defineRequestValidator(entityPathParams);
+
+export const publish = defineRequestValidator(entityPathParams);
+
+export const clone = defineRequestValidator([
+  ...entityPathParams,
+  // Target location
+  body('repositoryId').isInt(),
+  body('parentId').isInt().optional(),
+  body('position').isFloat().optional(),
+]);
+
+export const getPreviewUrl = defineRequestValidator(entityPathParams);
+
+export const updateWorkflowStatus = defineRequestValidator([
+  ...entityPathParams,
+  body('assigneeId').isInt().optional(),
+  body('status').isString().trim().escape().notEmpty().optional(),
+  body('priority').isString().trim().escape().notEmpty().optional(),
+  body('description').isString().trim().escape().optional(),
+  body('dueDate').isDate().optional(),
+]);
--- apps/backend/activity/index.js
+++ apps/backend/activity/index.js
@@ -4,6 +4,7 @@ import ctrl from './activity.controller.js';
 import db from '#shared/database/index.js';
 import { createError } from '#shared/error/helpers.js';
 import processListQuery from '#shared/util/processListQuery.js';
+import * as validation from '#activity/activity.validation.js';
 
 const { Activity } = db;
 const router = express.Router();
@@ -14,23 +15,30 @@ router.param('activityId', getActivity);
 router.route('/').get(processQuery, ctrl.list).post(ctrl.create);
 
 router
-  .route('/:activityId')
-  .get(ctrl.show)
-  .patch(ctrl.patch)
-  .delete(ctrl.remove);
+  .route('/:activityId', validation.get)
+  .get(ctrl.show, validation.get)
+  .patch(ctrl.patch, validation.update)
+  .delete(ctrl.remove, validation.remove);
 
 router
-  .patch('/:activityId/restore', ctrl.restore)
-  .post('/:activityId/reorder', ctrl.reorder)
-  .post('/:activityId/clone', ctrl.clone)
-  .post('/:activityId/status', ctrl.updateStatus)
-  .get('/:activityId/publish', ctrl.publish)
-  .get('/:activityId/preview', ctrl.getPreviewUrl);
+  .get('/:activityId/preview', validation.getPreviewUrl, ctrl.getPreviewUrl)
+  .get('/:activityId/publish', validation.publish, ctrl.publish)
+  .patch('/:activityId/restore', validation.restore, ctrl.restore)
+  .post('/:activityId/reorder', validation.reorder, ctrl.reorder)
+  .post('/:activityId/clone', validation.clone, ctrl.clone)
+  .post(
+    '/:activityId/status',
+    validation.updateWorkflowStatus,
+    ctrl.updateStatus,
+  );
 
 function getActivity(req, _res, next, activityId) {
   return Activity.findByPk(activityId, { paranoid: false })
     .then((it) => it || createError(StatusCodes.NOT_FOUND, 'Not found'))
     .then((activity) => {
+      if (activity.repositoryId !== req.repository.id) {
+        return createError(StatusCodes.FORBIDDEN, 'Access restricted');
+      }
       req.activity = activity;
       next();
     });
