fb0d08bf311488cecf721af3aac9066e4bcc8725
underscope <underscope@gmail.com>
2025-03-06 11:50:52 +0100

    Improve revision req validation

--- apps/backend/revision/index.js
+++ apps/backend/revision/index.js
@@ -1,35 +1,50 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
-import ctrl from './revision.controller.js';
-import db from '#shared/database/index.js';
+
+import * as validation from '#app/revision/revision.validation.js';
 import { createError } from '#shared/error/helpers.js';
+import ctrl from '#app/revision/revision.controller.js';
+import db from '#shared/database/index.js';
 import processListQuery from '#shared/util/processListQuery.js';
 
 const { NOT_FOUND } = StatusCodes;
-const { Revision, User } = db;
+const { Activity, Revision, User } = db;
+
 const router = express.Router();
-const defaultListQuery = {
-  order: [['createdAt', 'DESC']],
-};
+const defaultListQuery = { order: [['createdAt', 'DESC']] };
 
 router.param('revisionId', getRevision);
 
 router
-  .get('/time-travel', ctrl.getStateAtMoment)
-  .get('/', processListQuery(defaultListQuery), ctrl.index)
-  .get('/:revisionId', ctrl.get);
+  .get('/', validation.list, processListQuery(defaultListQuery), ctrl.index)
+  .get(
+    '/time-travel',
+    validation.getStateAtMoment,
+    loadTargetActivity,
+    ctrl.getStateAtMoment,
+  )
+  .get('/:revisionId', validation.get, ctrl.get);
 
 function getRevision(req, _res, next, revisionId) {
   const id = parseInt(revisionId, 10);
   const include = [{ model: User, attributes: ['id', 'email'] }];
-  return Revision.fetch(id, { include })
-    .then(
-      (revision) => revision || createError(NOT_FOUND, 'Revision not found'),
-    )
-    .then((revision) => {
-      req.revision = revision;
-      next();
-    });
+  return Revision.fetch(id, { include }).then((val) => {
+    if (!val || val.repositoryId !== req.repository.id) {
+      return createError(NOT_FOUND, 'Revision not found');
+    }
+    req.revision = val;
+    next();
+  });
+}
+
+function loadTargetActivity(req, _res, next) {
+  const { activityId } = req.query;
+  return Activity.findByPk(activityId).then((activity) => {
+    if (!activity || !activity.repositoryId === req.repository?.id)
+      return createError(NOT_FOUND, 'Activity not found');
+    req.activity = activity;
+    next();
+  });
 }
 
 export default {
