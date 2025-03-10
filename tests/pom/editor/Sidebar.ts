7fde69ce9a9825cd939f2b76a6a92e7c4a2e975b
rkusan00 <renato.kusanovic@gmail.com>
2025-03-10 14:11:25 +0100

    Extract navigation add tabs

--- tests/pom/editor/Sidebar.ts
+++ tests/pom/editor/Sidebar.ts
@@ -1,39 +1,54 @@
 import type { Locator, Page } from '@playwright/test';
-import { expect } from '@playwright/test';
+
+import { ActivityComments } from '../common/Comments';
+import { Navigation } from './Navigation';
+
+enum SidebarTab {
+  Comments = 'COMMENTS_TAB',
+  Browser = 'BROWSER_TAB',
+  Element = 'ELEMENT_TAB',
+}
 
 export class EditorSidebar {
   readonly page: Page;
   readonly el: Locator;
+  readonly tabs: Locator;
   readonly searchInput: Locator;
   readonly toggleAllBtn: Locator;
   readonly treeView: Locator;
+  readonly navigation: Navigation;
+  readonly comments: ActivityComments;
 
   constructor(page: Page) {
-    const el = page.locator('.sidebar-container');
-    this.searchInput = el.getByLabel('Search...');
-    this.toggleAllBtn = el.getByRole('button', { name: 'Toggle all' });
-    this.treeView = el.locator('.tree-view');
     this.page = page;
+    const el = page.locator('.sidebar-container');
     this.el = el;
+    this.tabs = el.getByRole('tablist');
+    this.navigation = new Navigation(page, el);
+    this.comments = new ActivityComments(page, el);
+  }
+
+  getTabByName(name: SidebarTab) {
+    return this.tabs.getByRole('tab', { name });
+  }
+
+  openCommentsTab() {
+    return this.getTabByName(SidebarTab.Comments).click();
+  }
+
+  openBrowserTab() {
+    return this.getTabByName(SidebarTab.Browser).click();
+  }
+
+  openElementTab() {
+    return this.getTabByName(SidebarTab.Element).click();
   }
 
   toggleItems() {
-    return this.toggleAllBtn.click();
+    return this.navigation.toggleItems();
   }
 
-  async getOutlineItems(name?: string) {
-    const el = this.el.locator('.v-list-item');
-    return name ? el.filter({ hasText: name }).all() : el.all();
-  }
-
-  async getOutlineItemByName(name: string) {
-    const items = await this.getOutlineItems(name);
-    return items[0];
-  }
-
-  async navigateTo(name: string) {
-    await expect(this.el).toBeVisible();
-    const item = await this.getOutlineItemByName(name);
-    await item.click();
+  navigateTo(name: string) {
+    return this.navigation.navigateTo(name);
   }
 }
