fcd2d8bbcd71f9e23fc597fb2bc7768ca06144e5
rkusan00 <renato.kusanovic@gmail.com>
2025-03-31 08:22:28 +0200

    Cleanup

--- apps/backend/shared/database/migrations/20250317092632-update-objectives-structure.js
+++ apps/backend/shared/database/migrations/20250317092632-update-objectives-structure.js
@@ -2,8 +2,10 @@
 
 const head = require('lodash/head');
 
+const ACTIVITY_TYPE = 'ASSESSMENT_GROUP';
+
 exports.up = async ({ sequelize }) => {
-  const activityIds = await getTargetActivities(sequelize);
+  const activityIds = await getActivityIdsByType(sequelize, ACTIVITY_TYPE);
   if (!activityIds.length) return;
   sequelize.query(`
     UPDATE content_element
@@ -17,25 +19,29 @@ exports.up = async ({ sequelize }) => {
   `);
 };
 
-exports.down = ({ sequelize }) =>
+exports.down = async ({ sequelize }) => {
+  const activityIds = await getActivityIdsByType(sequelize, ACTIVITY_TYPE);
+  if (!activityIds.length) return;
   sequelize.query(`
-  UPDATE content_element
-  SET refs = jsonb_set(
-    refs - 'objective',
-    '{objectiveId}',
-    refs->'objective'->'id'
-  )
-  WHERE refs ? 'objective';
-`);
+    UPDATE content_element
+    SET refs = jsonb_set(
+      refs - 'objective',
+      '{objectiveId}',
+      refs->'objective'->'id'
+    )
+    WHERE refs ? 'objective'
+    AND activity_id IN (${activityIds.join(',')});
+  `);
+};
 
-async function getTargetActivities(sequelize) {
+async function getActivityIdsByType(sequelize, type) {
   const sql = `
     SELECT
       id
     FROM
       activity
     WHERE
-      type = 'ASSESSMENT_GROUP';
+      type = '${type}';
   `;
   return head(await sequelize.query(sql, { raw: true })).map((row) => row.id);
 }
