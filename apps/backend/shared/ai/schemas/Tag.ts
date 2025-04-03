c00bed681b0bba1ce9fc1cea3d1cd17d1be01de4
underscope <underscope@gmail.com>
2025-04-03 15:07:48 +0200

    Add tag prompt

--- apps/backend/shared/ai/schemas/Tag.ts
+++ apps/backend/shared/ai/schemas/Tag.ts
@@ -6,12 +6,21 @@ export const Schema: OpenAISchema = {
   schema: {
     type: 'array',
     minItems: 2,
+    maxItems: 15,
     items: {
       type: 'string',
+      maxLength: 30,
     },
   },
 };
 
+export const Prompt = `
+  Return response as JSON and use the following format:
+  ['Example tag a', 'Example tag b', 'Example tag c'].
+  The response should not include more than 15 tags. Each tag should be in
+  human readable format and should not exceed 30 characters.`;
+
 export default {
   Schema,
+  Prompt,
 };
