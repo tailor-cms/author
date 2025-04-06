245905be53eee7e424bdac1648f77c8579a8fe97
underscope <underscope@gmail.com>
2025-04-06 21:27:40 +0200

    Improve ai interface
    - Add target audience
    - Add response schema

--- packages/tailor-interfaces/ai.ts
+++ packages/tailor-interfaces/ai.ts
@@ -1,8 +1,18 @@
-export type AiRequestType = 'ADD' | 'MODIFY';
+export type AiRequestType = 'CREATE' | 'ADD' | 'MODIFY';
+export type AiTargetAudience = 'BEGINNER' | 'INTERMEDIATE' | 'EXPERT';
+export type AiResponseSchema =
+  | 'HTML'
+  | 'QUESTION'
+  | 'OUTLINE'
+  | 'TAG'
+  | 'CUSTOM';
 
 export interface AiInput {
   type: AiRequestType;
   text: string;
+  responseSchema: AiResponseSchema;
+  targetAudience?: AiTargetAudience;
+  useImageGenerationTool?: boolean;
   useSearchTool?: boolean;
 }
 
@@ -10,14 +20,22 @@ export interface AiRepositoryContext {
   schemaId: string;
   name: string;
   description: string;
+  // If content is generated for a specific outline node
   outlineActivityType?: string;
+  // If content is generated for a specific outline node
   outlineLocation?: string;
+  // If content is generated for a container
   containerType?: string;
+  // If content is generated for a container
+  containerRules?: any;
+  // General topic of the content, in case of a outline node title of the leaf
   topic?: string;
+  // Additional information about the content
+  tags?: string[];
 }
 
 export interface AiContext {
-  repository?: AiRepositoryContext;
+  repository: AiRepositoryContext;
   content?: string;
   inputs: AiInput[];
 }
