b935add60aa2ae0b4454d9ec35bfb819a7d1d57c
underscope <underscope@gmail.com>
2025-04-15 15:13:29 +0200

    Update verbiage

--- tests/specs/functional/repository/settings.default-user.spec.ts
+++ tests/specs/functional/repository/settings.default-user.spec.ts
@@ -11,18 +11,18 @@ test.beforeEach(async ({ page }) => {
   await toSeededRepositorySettings(page, DEFAULT_TEST_USER.email);
 });
 
-test('should be able to delete assigned repository', async ({ page }) => {
+test('should be able to delete owned repository', async ({ page }) => {
   const settingsPage = new GeneralSettings(page);
   await settingsPage.sidebar.delete();
   await expect(page.getByText('0 available repositories')).toBeVisible();
 });
 
-test('should be able to export assigned repository', async ({ page }) => {
+test('should be able to export owned repository', async ({ page }) => {
   const settingsPage = new GeneralSettings(page);
   await settingsPage.sidebar.export();
 });
 
-test('should be able to clone assigned repository', async ({ page }) => {
+test('should be able to clone owned repository', async ({ page }) => {
   const settingsPage = new GeneralSettings(page);
   const name = 'Cloned Repository';
   await settingsPage.sidebar.clone(name);
@@ -30,7 +30,7 @@ test('should be able to clone assigned repository', async ({ page }) => {
   await expect(page.getByText(name)).toBeVisible();
 });
 
-test('should be able to publish assigned repository', async ({ page }) => {
+test('should be able to publish owned repository', async ({ page }) => {
   const settingsPage = new GeneralSettings(page);
   await settingsPage.sidebar.publish();
   const repositoryName = await settingsPage.getName();
@@ -42,21 +42,21 @@ test('should be able to publish assigned repository', async ({ page }) => {
   await expect(card.getByLabel('Has unpublished changes')).not.toBeVisible();
 });
 
-test('should be able to edit assigned repository name', async ({ page }) => {
+test('should be able to edit owned repository name', async ({ page }) => {
   const settingsPage = new GeneralSettings(page);
   await settingsPage.updateName('New Name');
   await page.reload();
   await expect(settingsPage.nameInput).toHaveValue('New Name');
 });
 
-test('should be able to edit assigned repository description', async ({ page }) => {
+test('should be able to edit owned repository description', async ({ page }) => {
   const settingsPage = new GeneralSettings(page);
   await settingsPage.updateDescription('New Description');
   await page.reload();
   await expect(settingsPage.descriptionInput).toHaveValue('New Description');
 });
 
-test('should be able to publish assigned repository info', async ({ page }) => {
+test('should be able to publish owned repository info', async ({ page }) => {
   const settingsPage = new GeneralSettings(page);
   await settingsPage.el.getByRole('button', { name: 'Publish info' }).click();
   await expect(page.getByText('Info successfully published')).toBeVisible();
