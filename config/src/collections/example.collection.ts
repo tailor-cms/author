356696582f1a23cea6ddc68f727176f32777a8e0
underscope <underscope@gmail.com>
2025-07-04 08:53:38 +0200

    Create example collection

---
+++ config/src/collections/example.collection.ts
@@ -0,0 +1,20 @@
+import type { ContentElement } from '@tailor-cms/interfaces/content-element.ts';
+import { MetaInputType } from '@tailor-cms/meta-element-collection/types.js';
+import { ContentElementType } from '@tailor-cms/content-element-collection/types.js';
+import { IsContentElement, IsInput, Prop, TailorCollection } from '../lib/index.ts';
+
+class Article {
+  @Prop()
+  @IsInput(MetaInputType.Textarea)
+  description: string;
+
+  @Prop()
+  @IsContentElement(ContentElementType.Html)
+  html: ContentElement;
+
+  @Prop({ label: 'Overview Video' })
+  @IsContentElement(ContentElementType.Video)
+  video: ContentElement;
+}
+
+export const exampleCollection = new TailorCollection(Article);
