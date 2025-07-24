be2e28f0ec12d36476c730e3b76b33760e306c8b
underscope <underscope@gmail.com>
2025-07-24 14:14:11 +0200

    Modify ListItem controls

--- apps/frontend/app/components/editor/Sidebar/TailorTreeview/ListItem.vue
+++ apps/frontend/app/components/editor/Sidebar/TailorTreeview/ListItem.vue
@@ -1,63 +1,72 @@
 <template>
-  <VListItem
-    ref="listItem"
-    v-bind="omit(activatorProps, 'onClick')"
-    :class="{ 'list-item-active': isActive, 'pt-2': isHovered }"
-    :create-context="createContext"
-    :title="title"
-    class="list-item"
-    @click.prevent="onItemClick"
-  >
-    <template #prepend>
-      <VBtn
-        v-if="isGroup"
-        :color="prependColor"
-        :icon="prependIcon"
-        class="ml-n1 mr-2"
-        density="comfortable"
-        variant="text"
-        @click.stop="activatorProps?.onClick"
-      />
-    </template>
-    <template #title>
-      <span :class="{ 'font-weight-bold': isActive }">{{ title }}</span>
-    </template>
-    <template #subtitle>
-      <VExpandTransition>
-        <ItemToolbar
-          v-if="isHovered || createContext?.activity?.id === id"
-          :id="id"
-          @create="emit('create', $event)"
-          @edit="emit('edit', $event)"
-        />
-      </VExpandTransition>
-    </template>
-    <template #append>
-      <div v-if="isHovered" class="d-flex flex-column align-center">
+  <VExpandTransition>
+    <VListItem
+      ref="listItem"
+      v-bind="omit(activatorProps, 'onClick')"
+      :class="{ 'list-item-active': isActive }"
+      :title="title"
+      class="list-item"
+      @click.prevent="onItemClick"
+    >
+      <template #prepend>
         <VBtn
-          color="primary-lighten-2"
-          icon="mdi-chevron-up"
-          density="compact"
+          v-if="isGroup"
+          :color="prependColor"
+          :icon="prependIcon"
+          class="ml-n1 mr-2"
+          density="comfortable"
           variant="text"
-          @click.stop="reoder(Direction.Up)"
+          @click.stop="activatorProps?.onClick"
         />
-        <VBtn
-          color="primary-lighten-2"
-          icon="mdi-chevron-down"
-          density="compact"
-          variant="text"
-          @click.stop="reoder(Direction.Down)"
-        />
-      </div>
-    </template>
-  </VListItem>
+      </template>
+      <template #title>
+        <div class="d-flex align-center">
+          <span :class="{ 'font-weight-bold': isActive }">{{ title }}</span>
+          <ActivityMenu
+            v-show="isHovered"
+            :activity="activity as any"
+            :activator="{
+              size: 'x-small',
+              variant: 'text',
+            }"
+            class="activity-menu ml-2"
+            @click.stop
+          />
+        </div>
+      </template>
+      <template #append>
+        <div v-if="isHovered" class="d-flex flex-column align-center">
+          <VBtn
+            :disabled="index === 0"
+            color="primary-lighten-2"
+            icon="mdi-chevron-up"
+            density="compact"
+            variant="text"
+            size="small"
+            @click.stop="reoder(Direction.Up)"
+          />
+          <VBtn
+            :disabled="index === siblings.length - 1"
+            color="primary-lighten-2"
+            icon="mdi-chevron-down"
+            density="compact"
+            variant="text"
+            size="small"
+            @click.stop="reoder(Direction.Down)"
+          />
+        </div>
+      </template>
+    </VListItem>
+  </VExpandTransition>
 </template>
 
 <script lang="ts" setup>
 import { computed, useTemplateRef } from 'vue';
 import { omit } from 'lodash-es';
 import { useElementHover } from '@vueuse/core';
-import ItemToolbar from './ItemToolbar.vue';
+import type { Activity } from '@tailor-cms/interfaces/activity';
+
+import ActivityMenu from '~/components/common/ActivityOptions/ActivityMenu.vue';
 
 const Direction = {
   Up: -1,
@@ -73,22 +82,21 @@ const props = defineProps<{
   isOpen?: boolean;
   isActive?: boolean;
   activatorProps?: Record<string, any>;
-  createContext?: { activity: any };
-  // Location of the item in the list
-  index: number;
-  // Siblings of the item in the list
-  siblings: any[];
+  index: number; // Location of the item in the list
+  siblings: any[]; // Siblings of the item in the list
 }>();
 
-const emit = defineEmits(['create', 'edit']);
+const emit = defineEmits(['edit']);
 
 const activityStore = useActivityStore();
 
 const listItem = useTemplateRef<HTMLButtonElement>('listItem');
 const isHovered = useElementHover(listItem, {
-  delayEnter: 800,
-  delayLeave: 2000,
+  delayEnter: 400,
+  delayLeave: 800,
 });
+
+const activity = activityStore.findById(props.id) as Activity;
 
 const prependIcon = computed(() => {
   if (!props.isGroup) return '';
@@ -107,9 +115,24 @@ const onItemClick = (e: any) => {
 
 const reoder = (direction: number) => {
   const activity = activityStore.findById(props.id) as any;
+  console.log('xxx', props.index + direction);
   return activityStore.reorder(activity, {
     items: props.siblings,
     position: props.index + direction,
   });
 };
 </script>
+
+<style lang="scss" scoped>
+.activity-menu {
+  :deep(.v-list-item-title),
+  :deep(.v-list-item-title:hover) {
+    color: rgb(var(--v-theme-primary-darken-3)) !important;
+    font-weight: 400 !important;
+  }
+}
+
+.v-list-item:hover .activity-menu :deep(.v-list-item__content) .v-list-item-title {
+  font-weight: 400 !important;
+}
+</style>
