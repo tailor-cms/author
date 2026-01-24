8baa6af5cae783a36d1e67be22b565c5a5deb99a
underscope <underscope@gmail.com>
2026-01-24 04:27:38 +0100

    Add linking to the activity menu

--- apps/frontend/app/components/common/ActivityOptions/ActivityMenu.vue
+++ apps/frontend/app/components/common/ActivityOptions/ActivityMenu.vue
@@ -47,6 +47,13 @@
       :repository-id="activity.repositoryId"
       @close="showCopyDialog = false"
     />
+    <LinkContent
+      v-if="showLinkDialog"
+      :action="linkAction"
+      :anchor="activity"
+      @close="showLinkDialog = false"
+      @completed="showLinkDialog = false"
+    />
   </div>
 </template>
 
@@ -57,6 +64,7 @@ import { InsertLocation } from '@tailor-cms/utils';
 
 import CopyDialog from '@/components/repository/Outline/CopyActivity/index.vue';
 import CreateDialog from '@/components/repository/Outline/CreateDialog/index.vue';
+import LinkContent from '@/components/repository/Library/LinkContent.vue';
 import { useCurrentRepository } from '@/stores/current-repository';
 import { useSelectedActivity } from '#imports';
 
@@ -71,8 +79,10 @@ const selectedActivity = useSelectedActivity(props.activity);
 
 const showCreateDialog = ref(false);
 const showCopyDialog = ref(false);
+const showLinkDialog = ref(false);
 const action = ref<InsertLocation>(AddAfter);
 const supportedLevels = ref<string[]>([]);
+const linkAction = ref<InsertLocation>(AddAfter);
 
 const addMenuOptions = computed(() => {
   const items = [
@@ -111,10 +121,27 @@ const copyMenuOptions = computed(() => {
   });
 });
 
+const linkMenuOptions = computed(() => {
+  const items = [
+    {
+      name: 'Link content below',
+      icon: 'mdi-link-variant',
+      action: () => setLinkContext(AddAfter),
+    },
+  ];
+  if (!selectedActivity.subLevels.value.length) return items;
+  return items.concat({
+    name: 'Link content into',
+    icon: 'mdi-link-variant',
+    action: () => setLinkContext(AddInto),
+  });
+});
+
 const menuOptions = computed(() => {
   return [
     ...addMenuOptions.value,
     ...copyMenuOptions.value,
+    ...linkMenuOptions.value,
     {
       name: 'Remove',
       icon: 'mdi-delete',
@@ -132,6 +159,11 @@ const setCopyContext = (levels: Activity[], actionValue: InsertLocation) => {
   supportedLevels.value = levels.map((it) => it.type);
   action.value = actionValue;
   showCopyDialog.value = true;
+};
+
+const setLinkContext = (actionValue: InsertLocation) => {
+  linkAction.value = actionValue;
+  showLinkDialog.value = true;
 };
 
 const deleteActivity = () => {
