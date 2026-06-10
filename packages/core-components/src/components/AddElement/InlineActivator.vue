aa69ea0c67656f6802aed76d65140311837c84e0
rkusan00 <renato.kusanovic@gmail.com>
2026-06-10 16:57:13 +0200

    Cleanup

--- packages/core-components/src/components/AddElement/InlineActivator.vue
+++ packages/core-components/src/components/AddElement/InlineActivator.vue
@@ -1,10 +1,10 @@
 <template>
-  <button :class="{ disabled }" class="inline-activator">
-    <VDivider content-offset="-16" opacity="0.5" gradient>
+  <button :class="{ disabled }" class="inline-activator d-block">
+    <VDivider content-offset="-16" opacity="0.5">
       <VAvatar
         color="surface-container-highest"
         icon="mdi-plus"
-        size="small"
+        size="20"
       />
     </VDivider>
   </button>
@@ -17,30 +17,16 @@ withDefaults(defineProps<{ disabled?: boolean }>(), {
 </script>
 
 <style lang="scss" scoped>
-.default-activator-state {
-  padding: 0 3.125rem;
-  opacity: 0;
-}
-
 .inline-activator {
-  @extend .default-activator-state;
-
-  display: flex;
-  align-items: center;
   width: 100%;
-  margin: 0;
   padding: 0 3.125rem;
   background: transparent;
   border: none;
   opacity: 0;
-  transition:
-    opacity 0.3s,
-    padding 0.3s;
+  transition: opacity 0.3s, padding 0.3s;
 
-  .v-divider__wrapper {
-    flex: 1;
-    display: inline-flex;
-    margin: 0;
+  :deep(.v-divider__content) {
+    line-height: 1;
   }
 
   &:hover,
@@ -48,14 +34,10 @@ withDefaults(defineProps<{ disabled?: boolean }>(), {
     padding: 0 0 1.25rem 0;
     opacity: 1;
     outline: none;
-    transition:
-      opacity 0.3s 0.25s,
-      padding 0.3s 0.1s;
+    transition: opacity 0.3s 0.25s, padding 0.3s 0.1s;
   }
 
-  &.disabled,
-  &.disabled:hover {
-    @extend .default-activator-state;
+  &.disabled {
     pointer-events: none;
   }
 }
