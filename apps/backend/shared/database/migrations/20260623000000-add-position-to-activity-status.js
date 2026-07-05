e5e6979602c9547abd4f9580aee3dcdb3a5ff72f
rkusan00 <renato.kusanovic@gmail.com>
2026-07-05 22:18:58 +0200

    Assign workflow position at activity creation

--- apps/backend/shared/database/migrations/20260623000000-add-position-to-activity-status.js
+++ apps/backend/shared/database/migrations/20260623000000-add-position-to-activity-status.js
@@ -3,7 +3,11 @@
 const TABLE_NAME = 'activity_status';
 const COLUMN_NAME = 'position';
 
-exports.up = (qi, { FLOAT }) =>
-  qi.addColumn(TABLE_NAME, COLUMN_NAME, { type: FLOAT });
+exports.up = async (qi, { FLOAT }) => {
+  await qi.addColumn(TABLE_NAME, COLUMN_NAME, { type: FLOAT });
+  await qi.sequelize.query(
+    `UPDATE ${TABLE_NAME} SET ${COLUMN_NAME} = activity_id WHERE ${COLUMN_NAME} IS NULL`,
+  );
+};
 
 exports.down = (qi) => qi.removeColumn(TABLE_NAME, COLUMN_NAME);
