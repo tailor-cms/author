ccbeb8e88e5b71fa42f731c8d5fc70f81e78d373
underscope <underscope@gmail.com>
2026-07-22 19:36:53 +0200

    Trim long group name and add tooltip

--- apps/frontend/app/components/catalog/AddRepository/index.vue
+++ apps/frontend/app/components/catalog/AddRepository/index.vue
@@ -129,6 +129,7 @@
           :clearable="!lockedGroupId"
           :error-messages="errors.userGroupIds"
           :items="authStore.groupsWithCreateRepositoryAccess"
+          :menu-props="{ maxWidth: 460 }"
           class="user-group-select mb-3"
           item-title="name"
           item-value="id"
@@ -143,9 +144,17 @@
         >
           <template #chip="{ internalItem, props: chipProps }">
             <VChip
+              v-tooltip:top="{
+                text: internalItem.title,
+                openDelay: 400,
+                maxWidth: 400,
+              }"
               v-bind="chipProps"
               :closable="internalItem.value !== lockedGroupId"
-            />
+              class="user-group-chip"
+            >
+              <span class="text-truncate">{{ internalItem.title }}</span>
+            </VChip>
           </template>
         </VSelect>
       </div>
