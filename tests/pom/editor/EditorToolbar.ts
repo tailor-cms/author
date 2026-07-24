a9ea17b5ac02bfef6b4cfd951c8c0df4e39efb7a
mbarisic0 <mbarisic@extensionengine.com>
2026-07-24 14:56:25 +0200

    Use ConfirmationDialog from POM

--- tests/pom/editor/EditorToolbar.ts
+++ tests/pom/editor/EditorToolbar.ts
@@ -2,6 +2,7 @@ import type { Locator, Page } from '@playwright/test';
 import { expect } from '@playwright/test';
 
 import { Toast } from '../common/Toast';
+import { ConfirmationDialog } from '../common/ConfirmationDialog';
 
 export class EditorToolbar {
   readonly page: Page;
@@ -12,6 +13,7 @@ export class EditorToolbar {
   readonly publishBtn: Locator;
   readonly compareBtn: Locator;
   readonly toast: Toast;
+  readonly confirmationDialog: ConfirmationDialog;
 
   constructor(page: Page) {
     this.page = page;
@@ -22,6 +24,8 @@ export class EditorToolbar {
     this.publishBtn = this.el.getByRole('button', { name: 'Publish', exact: true });
     this.compareBtn = this.el.getByRole('button', { name: 'Compare' });
     this.toast = new Toast(page);
+    this.confirmationDialog = new ConfirmationDialog(page,
+      'Are you sure you want to publish the Page');
   }
 
   async expectLinkedState() {
@@ -57,8 +61,7 @@ export class EditorToolbar {
   // Publishes the activity and asserts the success toast
   async publishAndVerify() {
     await this.publishBtn.click();
-    const dialog = this.page.locator('div[role="dialog"]');
-    await dialog.getByRole('button', { name: 'confirm' }).click();
+    await this.confirmationDialog.confirm();
     await this.toast.hasText(/has been published/i);
   }
 
